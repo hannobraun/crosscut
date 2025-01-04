fb1055069569a72db472f81938636c13aa2a627c
Hanno Braun <hello@hannobraun.com>
2025-01-04 23:20:59 +0100

    Prepare to send data from `game_io` to `language`

--- prototype/src/language.rs
+++ prototype/src/language.rs
@@ -3,22 +3,28 @@ use std::{
     thread,
 };
 
+use tokio::runtime::Runtime;
+
 pub fn start_in_background() -> anyhow::Result<GameIo> {
+    let runtime = Runtime::new()?;
+
     let (color_tx, color_rx) = mpsc::sync_channel(0);
 
     thread::spawn(move || {
-        let color = [0., 0., 0., 1.];
+        runtime.block_on(async {
+            let color = [0., 0., 0., 1.];
 
-        println!("Color: {color:?}");
+            println!("Color: {color:?}");
 
-        loop {
-            // The channel has no buffer, so this is synchronized to the frame
-            // rate of the renderer.
-            if let Err(SendError(_)) = color_tx.send(color) {
-                // The other end has hung up. Time for us to shut down too.
-                break;
+            loop {
+                // The channel has no buffer, so this is synchronized to the
+                // frame rate of the renderer.
+                if let Err(SendError(_)) = color_tx.send(color) {
+                    // The other end has hung up. Time for us to shut down too.
+                    break;
+                }
             }
-        }
+        });
     });
 
     Ok(GameIo { output: color_rx })
