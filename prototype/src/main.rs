bcdbef10e9ca81659e857683fdcbacfb810508f5
Hanno Braun <hello@hannobraun.com>
2025-01-04 20:35:45 +0100

    Update name of variable

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -2,7 +2,7 @@ mod application;
 mod language;
 
 fn main() -> anyhow::Result<()> {
-    let color = language::start_in_background();
-    application::start_and_block(color)?;
+    let updates = language::start_in_background();
+    application::start_and_block(updates)?;
     Ok(())
 }
