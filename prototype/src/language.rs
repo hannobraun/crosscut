70b8f31b693c9ddabab59606bfb063cda1ea08d8
Hanno Braun <hello@hannobraun.com>
2025-01-04 14:28:29 +0100

    Make name of function more explicit

--- prototype/src/language.rs
+++ prototype/src/language.rs
@@ -1,6 +1,6 @@
 use tokio::sync::watch;
 
-pub fn start() -> watch::Receiver<[f64; 4]> {
+pub fn start_in_background() -> watch::Receiver<[f64; 4]> {
     let (_, color_rx) = watch::channel([0., 0., 0., 1.]);
     color_rx
 }
--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -2,7 +2,7 @@ mod application;
 mod language;
 
 fn main() -> anyhow::Result<()> {
-    let color = language::start();
+    let color = language::start_in_background();
     application::start(color)?;
     Ok(())
 }
