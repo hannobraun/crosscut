b30f90049889d2233e4fe8a0f3bde1e20409562a
Hanno Braun <hello@hannobraun.com>
2025-01-04 23:08:18 +0100

    Update name of variable

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -2,7 +2,7 @@ mod game_io;
 mod language;
 
 fn main() -> anyhow::Result<()> {
-    let updates = language::start_in_background();
-    game_io::start_and_wait(updates.output)?;
+    let game_io = language::start_in_background();
+    game_io::start_and_wait(game_io.output)?;
     Ok(())
 }
