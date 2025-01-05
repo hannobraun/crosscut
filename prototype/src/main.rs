4029cb543de1537f389f6b4a873951f7c7b7538b
Hanno Braun <hello@hannobraun.com>
2025-01-05 22:41:13 +0100

    Make name of variable more explicit

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,14 +6,14 @@ mod language;
 fn main() -> anyhow::Result<()> {
     let (game_output_tx, game_output_rx) = actor::channel();
 
-    let (mut language, mut commands, mut input) =
+    let (mut language, mut commands, mut game_input) =
         language::start(game_output_tx)?;
     let mut cli = cli::start(commands.sender);
-    game_io::start_and_wait(input.sender, game_output_rx)?;
+    game_io::start_and_wait(game_input.sender, game_output_rx)?;
 
     language.join()?;
     commands.handle.join()?;
-    input.handle.join()?;
+    game_input.handle.join()?;
     cli.join()?;
 
     Ok(())
