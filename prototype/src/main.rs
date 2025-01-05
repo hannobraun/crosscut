dd78b54320ba5c219c8603299f098a93c969482b
Hanno Braun <hello@hannobraun.com>
2025-01-05 22:39:12 +0100

    Update name of variable

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -4,9 +4,10 @@ mod game_io;
 mod language;
 
 fn main() -> anyhow::Result<()> {
-    let (color_tx, color_rx) = actor::channel();
+    let (game_output_tx, color_rx) = actor::channel();
 
-    let (mut language, mut commands, mut input) = language::start(color_tx)?;
+    let (mut language, mut commands, mut input) =
+        language::start(game_output_tx)?;
     let mut cli = cli::start(commands.sender);
     game_io::start_and_wait(input.sender, color_rx)?;
 
