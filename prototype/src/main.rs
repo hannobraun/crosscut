48838d1f24eeeda48b4f8b32c8690b76624c873c
Hanno Braun <hello@hannobraun.com>
2025-01-05 22:16:30 +0100

    Improve panic handling

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,10 +6,11 @@ mod language;
 fn main() -> anyhow::Result<()> {
     let (color_tx, color_rx) = actor::channel();
 
-    let (_, mut commands, mut input) = language::start(color_tx)?;
+    let (mut language, mut commands, mut input) = language::start(color_tx)?;
     let mut cli = cli::start(commands.sender);
     game_io::start_and_wait(input.sender, color_rx)?;
 
+    language.join()?;
     commands.handle.join()?;
     input.handle.join()?;
     cli.join()?;
