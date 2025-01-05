de469efa8c855f37a1bdaa0af6fc6e501725c6a1
Hanno Braun <hello@hannobraun.com>
2025-01-05 22:12:13 +0100

    Improve panic handling for some threads

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,10 +6,12 @@ mod language;
 fn main() -> anyhow::Result<()> {
     let (color_tx, color_rx) = actor::channel();
 
-    let (input, commands) = language::start(color_tx)?;
+    let (mut input, mut commands) = language::start(color_tx)?;
     let mut cli = cli::start(commands.sender);
     game_io::start_and_wait(input.sender, color_rx)?;
 
+    input.handle.join()?;
+    commands.handle.join()?;
     cli.join()?;
 
     Ok(())
