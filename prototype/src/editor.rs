9137b161efe66016dfb9b2b9f18248f672dbfa4b
Hanno Braun <hello@hannobraun.com>
2025-01-06 15:53:44 +0100

    Prepare to render interpreter state

--- prototype/src/editor.rs
+++ prototype/src/editor.rs
@@ -1,23 +1,25 @@
+use std::io::{self, stdout};
+
 use crate::{
     code::model::{Code, Expression},
     interpreter::Interpreter,
 };
 
 pub fn update(code: &Code, _: &Interpreter) -> anyhow::Result<()> {
-    render_code(code)?;
+    render_code(code, stdout())?;
     Ok(())
 }
 
-fn render_code(code: &Code) -> anyhow::Result<()> {
+fn render_code(code: &Code, mut w: impl io::Write) -> anyhow::Result<()> {
     for expression in &code.expressions {
-        print!("    ");
+        write!(w, "    ")?;
 
         match expression {
             Expression::LiteralNumber { value } => {
-                println!("{value}");
+                writeln!(w, "{value}")?;
             }
             Expression::InvalidNumber { invalid } => {
-                println!("invalid number `{invalid}`");
+                writeln!(w, "invalid number `{invalid}`")?;
             }
         }
     }
