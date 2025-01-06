d566a363641254a576aba4c69fc6ae144fb70e36
Hanno Braun <hello@hannobraun.com>
2025-01-06 14:48:45 +0100

    Implement `Display` for `Code`

--- prototype/src/code/model.rs
+++ prototype/src/code/model.rs
@@ -1,9 +1,29 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub struct Code {
     pub expressions: Vec<Expression>,
 }
 
+impl fmt::Display for Code {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for expression in &self.expressions {
+            writeln!(f, "{expression}")?;
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Debug)]
 pub enum Expression {
     LiteralNumber { value: f64 },
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expression::LiteralNumber { value } => write!(f, "{value}"),
+        }
+    }
 }
