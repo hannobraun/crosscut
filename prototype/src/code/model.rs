75b8c012c222e096447164044024567fcb644c4d
Hanno Braun <hello@hannobraun.com>
2025-01-06 15:36:48 +0100

    Prepare to render interpreter state

--- prototype/src/code/model.rs
+++ prototype/src/code/model.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 #[derive(Debug, Default)]
 pub struct Code {
     pub expressions: Vec<Expression>,
@@ -9,15 +7,4 @@ pub struct Code {
 pub enum Expression {
     LiteralNumber { value: f64 },
     InvalidNumber { invalid: String },
-}
-
-impl fmt::Display for Expression {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Expression::LiteralNumber { value } => write!(f, "{value}"),
-            Expression::InvalidNumber { invalid } => {
-                write!(f, "invalid number `{invalid}`")
-            }
-        }
-    }
 }
--- prototype/src/editor.rs
+++ prototype/src/editor.rs
@@ -1,7 +1,14 @@
-use crate::code::model::Code;
+use crate::code::model::{Code, Expression};
 
 pub fn update(code: &Code) {
     for expression in &code.expressions {
-        println!("{expression}");
+        match expression {
+            Expression::LiteralNumber { value } => {
+                println!("{value}");
+            }
+            Expression::InvalidNumber { invalid } => {
+                println!("invalid number `{invalid}`");
+            }
+        }
     }
 }
