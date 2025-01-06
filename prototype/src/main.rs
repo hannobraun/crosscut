09ddd93001c2022be6c7beea477917c24b2c863a
Hanno Braun <hello@hannobraun.com>
2025-01-06 14:29:46 +0100

    Update name of module

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,15 +1,15 @@
 mod actor;
 mod code;
-mod editor;
 mod game_io;
 mod language;
+mod stdin;
 
 fn main() -> anyhow::Result<()> {
     let (game_output_tx, game_output_rx) = actor::channel();
 
     let (mut language, mut commands, mut game_input) =
         language::start(game_output_tx)?;
-    let mut editor = editor::start(commands.sender);
+    let mut editor = stdin::start(commands.sender);
     game_io::start_and_wait(game_input.sender, game_output_rx)?;
 
     language.join()?;
