c971035feb21417d3dfe577a23b46c8e5602e7f5
Hanno Braun <hello@hannobraun.com>
2025-01-06 15:38:08 +0100

    Prepare to render interpreter state

--- prototype/src/editor.rs
+++ prototype/src/editor.rs
@@ -2,6 +2,8 @@ use crate::code::model::{Code, Expression};
 
 pub fn update(code: &Code) {
     for expression in &code.expressions {
+        print!("    ");
+
         match expression {
             Expression::LiteralNumber { value } => {
                 println!("{value}");
