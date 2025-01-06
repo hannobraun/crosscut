480e492df93e4859ce974f87e950d1c30de2d2f5
Hanno Braun <hello@hannobraun.com>
2025-01-06 14:16:12 +0100

    Update name of function parameter

--- prototype/src/editor.rs
+++ prototype/src/editor.rs
@@ -2,9 +2,9 @@ use std::io::stdin;
 
 use crate::actor::{Actor, ActorHandle, Sender};
 
-pub fn start(commands: Sender<String>) -> ActorHandle {
+pub fn start(lines: Sender<String>) -> ActorHandle {
     Actor::spawn(move |line| {
-        commands.send(line)?;
+        lines.send(line)?;
 
         Ok(())
     })
