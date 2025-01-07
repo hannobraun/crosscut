c2d0c58d838de6caa02c3d0c5da9326ec093f84c
Hanno Braun <hello@hannobraun.com>
2025-01-07 12:19:37 +0100

    Prepare for follow-on change

--- prototype/src/language/compiler.rs
+++ prototype/src/language/compiler.rs
@@ -4,9 +4,10 @@ pub fn compile(input: String, code: &mut Code) {
     for token in input.split_whitespace() {
         let expression = match token.parse::<f64>() {
             Ok(value) => Expression::LiteralNumber { value },
-            Err(_) => Expression::Identifier {
-                name: token.to_string(),
-            },
+            Err(_) => {
+                let name = token.to_string();
+                Expression::Identifier { name }
+            }
         };
 
         code.expressions.push(expression);
