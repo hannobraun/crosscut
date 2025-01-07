f85727e9a7d0e50ca8201c3b935e6db90d461385
Hanno Braun <hello@hannobraun.com>
2025-01-07 11:58:10 +0100

    Prepare for follow-on change

--- prototype/src/language/compiler.rs
+++ prototype/src/language/compiler.rs
@@ -1,13 +1,16 @@
 use super::code::Expression;
 
 pub fn compile(input: String) -> Vec<Expression> {
-    input
-        .split_whitespace()
-        .map(|token| match token.parse::<f64>() {
+    let mut expressions = Vec::new();
+
+    expressions.extend(input.split_whitespace().map(|token| {
+        match token.parse::<f64>() {
             Ok(value) => Expression::LiteralNumber { value },
             Err(_) => Expression::Identifier {
                 name: token.to_string(),
             },
-        })
-        .collect()
+        }
+    }));
+
+    expressions
 }
