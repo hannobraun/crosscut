cedcab1622354df0576547d085bcd6c56850bfdb
Hanno Braun <hello@hannobraun.com>
2025-01-07 11:59:02 +0100

    Simplify

--- prototype/src/language/compiler.rs
+++ prototype/src/language/compiler.rs
@@ -3,14 +3,16 @@ use super::code::Expression;
 pub fn compile(input: String) -> Vec<Expression> {
     let mut expressions = Vec::new();
 
-    expressions.extend(input.split_whitespace().map(|token| {
-        match token.parse::<f64>() {
+    for token in input.split_whitespace() {
+        let expression = match token.parse::<f64>() {
             Ok(value) => Expression::LiteralNumber { value },
             Err(_) => Expression::Identifier {
                 name: token.to_string(),
             },
-        }
-    }));
+        };
+
+        expressions.push(expression);
+    }
 
     expressions
 }
