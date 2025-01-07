853e517bac327efbac28adfe19b98456a1fa64d1
Hanno Braun <hello@hannobraun.com>
2025-01-07 19:50:00 +0100

    Don't needlessly return, if nothing was executed

--- prototype/src/language/interpreter.rs
+++ prototype/src/language/interpreter.rs
@@ -16,44 +16,49 @@ impl Interpreter {
     }
 
     pub fn step(&mut self, code: &Code) -> Option<(usize, f64)> {
-        let index = self.next_expression;
-        let expression = self.next_expression(code)?;
+        loop {
+            let index = self.next_expression;
+            let expression = self.next_expression(code)?;
 
-        if let Some(HostFunction {
-            id,
-            signature:
-                Signature {
-                    input: (),
-                    output: (),
-                },
-        }) = self.active_function
-        {
-            match expression {
-                Expression::Identifier { .. } => {
-                    // Function call is already in progress, and nested function
-                    // calls are not supported yet.
-                }
-                Expression::LiteralNumber { value } => {
-                    self.active_function = None;
-                    self.next_expression += 1;
-                    return Some((id, *value));
-                }
-            }
-        } else {
-            match expression {
-                Expression::Identifier { .. } => {
-                    if let Some(function) =
-                        code.function_calls.get(&index).copied()
-                    {
-                        self.active_function = Some(function);
+            if let Some(HostFunction {
+                id,
+                signature:
+                    Signature {
+                        input: (),
+                        output: (),
+                    },
+            }) = self.active_function
+            {
+                match expression {
+                    Expression::Identifier { .. } => {
+                        // Function call is already in progress, and nested
+                        // function calls are not supported yet.
+                    }
+                    Expression::LiteralNumber { value } => {
+                        self.active_function = None;
                         self.next_expression += 1;
+                        return Some((id, *value));
                     }
                 }
-                Expression::LiteralNumber { .. } => {
-                    // There's not function call in progress, and thus nowhere
-                    // to put a value right now.
+            } else {
+                match expression {
+                    Expression::Identifier { .. } => {
+                        if let Some(function) =
+                            code.function_calls.get(&index).copied()
+                        {
+                            self.active_function = Some(function);
+                            self.next_expression += 1;
+                            continue;
+                        }
+                    }
+                    Expression::LiteralNumber { .. } => {
+                        // There's not function call in progress, and thus
+                        // nowhere to put a value right now.
+                    }
                 }
             }
+
+            break;
         }
 
         None
