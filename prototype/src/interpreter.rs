4f34b560d326038b139224412064651e04742d29
Hanno Braun <hello@hannobraun.com>
2025-01-07 10:56:35 +0100

    Organize interpreter step by state

--- prototype/src/interpreter.rs
+++ prototype/src/interpreter.rs
@@ -20,19 +20,23 @@ impl Interpreter {
     pub fn step(&mut self, code: &Code) -> Option<f64> {
         let expression = code.expressions.get(self.next_expression)?;
 
-        match expression {
-            Expression::Identifier { name } => {
-                if self.functions.contains(name) && !self.active_function {
-                    self.active_function = true;
+        if self.active_function {
+            match expression {
+                Expression::Identifier { .. } => {}
+                Expression::LiteralNumber { value } => {
                     self.next_expression += 1;
-                }
-            }
-            Expression::LiteralNumber { value } => {
-                if self.active_function {
-                    self.next_expression += 1;
-
                     return Some(*value);
                 }
+            }
+        } else {
+            match expression {
+                Expression::Identifier { name } => {
+                    if self.functions.contains(name) {
+                        self.active_function = true;
+                        self.next_expression += 1;
+                    }
+                }
+                Expression::LiteralNumber { .. } => {}
             }
         }
 
