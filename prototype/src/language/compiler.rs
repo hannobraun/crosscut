858c3975978179ec09450a37b760a3ab651b01e6
Hanno Braun <hello@hannobraun.com>
2025-01-07 11:57:07 +0100

    Simplify name of function parameter

--- prototype/src/language/compiler.rs
+++ prototype/src/language/compiler.rs
@@ -1,7 +1,7 @@
 use super::code::Expression;
 
-pub fn compile(input_code: String) -> Vec<Expression> {
-    input_code
+pub fn compile(input: String) -> Vec<Expression> {
+    input
         .split_whitespace()
         .map(|token| match token.parse::<f64>() {
             Ok(value) => Expression::LiteralNumber { value },
