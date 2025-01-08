bcb318ec70ed7a48656ddb77b9c88b9eff22e9a2
Hanno Braun <hello@hannobraun.com>
2025-01-08 13:52:06 +0100

    Fix editor showing stale state

--- prototype/src/language/other.rs
+++ prototype/src/language/other.rs
@@ -21,24 +21,25 @@ pub fn start(
         match event {
             Event::EditorInput { line } => {
                 compile(&line, &mut code);
+
+                match interpreter.step(&code) {
+                    InterpreterState::Error => {
+                        // Not handling errors right now. Eventually, those
+                        // should be properly encoded in `Code` and therefore
+                        // visible in the editor. But in any case, there's
+                        // nothing to do here, at least for now.
+                    }
+                    InterpreterState::Finished { output } => {
+                        game_output.send(GameOutput::SubmitColor {
+                            color: [output, output, output, 1.],
+                        })?;
+                    }
+                }
+
                 editor::update(&code, &interpreter)?;
             }
             Event::GameInput(GameInput::RenderingFrame) => {
                 // This loop is coupled to the frame rate of the renderer.
-            }
-        }
-
-        match interpreter.step(&code) {
-            InterpreterState::Error => {
-                // Not handling errors right now. Eventually, those should be
-                // properly encoded in `Code` and therefore visible in the
-                // editor. But in any case, there's nothing to do here, at least
-                // for now.
-            }
-            InterpreterState::Finished { output } => {
-                game_output.send(GameOutput::SubmitColor {
-                    color: [output, output, output, 1.],
-                })?;
             }
         }
 
