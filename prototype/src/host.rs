102e5b835f1c192b734ca4802bf1144add061224
Hanno Braun <hello@hannobraun.com>
2025-01-08 14:05:42 +0100

    Convert free function into constructor

--- prototype/src/host.rs
+++ prototype/src/host.rs
@@ -14,58 +14,60 @@ pub struct Host {
     pub handle_game_input: Actor<GameInput>,
 }
 
-pub fn start(game_output: Sender<GameOutput>) -> anyhow::Result<Host> {
-    let mut code = Code::default();
-    let mut interpreter = Interpreter::default();
+impl Host {
+    pub fn start(game_output: Sender<GameOutput>) -> anyhow::Result<Host> {
+        let mut code = Code::default();
+        let mut interpreter = Interpreter::default();
 
-    editor::update(&code, &interpreter)?;
+        editor::update(&code, &interpreter)?;
 
-    let handle_events = Actor::spawn(move |event| {
-        match event {
-            Event::EditorInput { line } => {
-                compile(&line, &mut code);
+        let handle_events = Actor::spawn(move |event| {
+            match event {
+                Event::EditorInput { line } => {
+                    compile(&line, &mut code);
 
-                match interpreter.step(&code) {
-                    InterpreterState::Error => {
-                        // Not handling errors right now. Eventually, those
-                        // should be properly encoded in `Code` and therefore
-                        // visible in the editor. But in any case, there's
-                        // nothing to do here, at least for now.
+                    match interpreter.step(&code) {
+                        InterpreterState::Error => {
+                            // Not handling errors right now. Eventually, those
+                            // should be properly encoded in `Code` and
+                            // therefore visible in the editor. But in any case,
+                            // there's nothing to do here, at least for now.
+                        }
+                        InterpreterState::Finished { output } => {
+                            game_output.send(GameOutput::SubmitColor {
+                                color: [output, output, output, 1.],
+                            })?;
+                        }
                     }
-                    InterpreterState::Finished { output } => {
-                        game_output.send(GameOutput::SubmitColor {
-                            color: [output, output, output, 1.],
-                        })?;
-                    }
+
+                    editor::update(&code, &interpreter)?;
                 }
-
-                editor::update(&code, &interpreter)?;
+                Event::GameInput(GameInput::RenderingFrame) => {
+                    // This loop is coupled to the frame rate of the renderer.
+                }
             }
-            Event::GameInput(GameInput::RenderingFrame) => {
-                // This loop is coupled to the frame rate of the renderer.
-            }
-        }
 
-        Ok(())
-    });
+            Ok(())
+        });
 
-    let events_from_editor_input = handle_events.sender.clone();
-    let handle_editor_input = Actor::spawn(move |line| {
-        events_from_editor_input.send(Event::EditorInput { line })?;
-        Ok(())
-    });
+        let events_from_editor_input = handle_events.sender.clone();
+        let handle_editor_input = Actor::spawn(move |line| {
+            events_from_editor_input.send(Event::EditorInput { line })?;
+            Ok(())
+        });
 
-    let events_from_game_input = handle_events.sender;
-    let handle_game_input = Actor::spawn(move |input| {
-        events_from_game_input.send(Event::GameInput(input))?;
-        Ok(())
-    });
+        let events_from_game_input = handle_events.sender;
+        let handle_game_input = Actor::spawn(move |input| {
+            events_from_game_input.send(Event::GameInput(input))?;
+            Ok(())
+        });
 
-    Ok(Host {
-        handle: handle_events.handle,
-        handle_editor_input,
-        handle_game_input,
-    })
+        Ok(Host {
+            handle: handle_events.handle,
+            handle_editor_input,
+            handle_game_input,
+        })
+    }
 }
 
 enum Event {
--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,9 +6,11 @@ mod language;
 mod stdin;
 
 fn main() -> anyhow::Result<()> {
+    use host::Host;
+
     let (game_output_tx, game_output_rx) = actor::channel();
 
-    let mut host = host::start(game_output_tx)?;
+    let mut host = Host::start(game_output_tx)?;
     let mut editor = stdin::start(host.handle_editor_input.sender);
     game_io::start_and_wait(host.handle_game_input.sender, game_output_rx)?;
 
