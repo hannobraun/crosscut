59151ebe5bf69fd762133af23a8b8a059c0eade5
Hanno Braun <hello@hannobraun.com>
2025-01-08 10:39:12 +0100

    Prepare further clean up of `Code`

--- prototype/src/language/interpreter.rs
+++ prototype/src/language/interpreter.rs
@@ -21,36 +21,38 @@ impl Interpreter {
             let expression = self.next_expression(code)?;
 
             match expression {
-                Token::Identifier { .. } => {
-                    if let Some(ActiveCall {
-                        target: HostFunction { id: _ },
-                    }) = self.active_call
-                    {
-                        // Function call is already in progress, and nested
-                        // function calls are not supported yet.
-                    } else if let Some(target) =
-                        code.function_calls.get(&index).copied()
-                    {
-                        self.active_call = Some(ActiveCall { target });
-                        self.next_expression += 1;
-                        continue;
-                    } else {
-                        // No function found. This identifier is unresolved.
+                Fragment::UnexpectedToken { token } => match token {
+                    Token::Identifier { .. } => {
+                        if let Some(ActiveCall {
+                            target: HostFunction { id: _ },
+                        }) = self.active_call
+                        {
+                            // Function call is already in progress, and nested
+                            // function calls are not supported yet.
+                        } else if let Some(target) =
+                            code.function_calls.get(&index).copied()
+                        {
+                            self.active_call = Some(ActiveCall { target });
+                            self.next_expression += 1;
+                            continue;
+                        } else {
+                            // No function found. This identifier is unresolved.
+                        }
                     }
-                }
-                Token::LiteralNumber { value } => {
-                    if let Some(ActiveCall {
-                        target: HostFunction { id },
-                    }) = self.active_call
-                    {
-                        self.active_call = None;
-                        self.next_expression += 1;
-                        return Some((id, *value));
-                    } else {
-                        // There's not function call in progress, and thus
-                        // nowhere to put a value right now.
+                    Token::LiteralNumber { value } => {
+                        if let Some(ActiveCall {
+                            target: HostFunction { id },
+                        }) = self.active_call
+                        {
+                            self.active_call = None;
+                            self.next_expression += 1;
+                            return Some((id, *value));
+                        } else {
+                            // There's not function call in progress, and thus
+                            // nowhere to put a value right now.
+                        }
                     }
-                }
+                },
             }
 
             break;
@@ -59,9 +61,8 @@ impl Interpreter {
         None
     }
 
-    pub fn next_expression<'r>(&self, code: &'r Code) -> Option<&'r Token> {
-        let Fragment::UnexpectedToken { token } =
-            code.fragments.get(self.next_expression)?;
+    pub fn next_expression<'r>(&self, code: &'r Code) -> Option<&'r Fragment> {
+        let token = code.fragments.get(self.next_expression)?;
         Some(token)
     }
 }
