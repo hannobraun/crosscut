08b4bb1a83c9e076fe5e938a272b617deb4bbefd
Hanno Braun <hello@hannobraun.com>
2025-01-08 12:21:53 +0100

    Derive `PartialEq` for `Code`

--- prototype/src/language/code.rs
+++ prototype/src/language/code.rs
@@ -1,29 +1,29 @@
 use std::collections::BTreeMap;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct Code {
     pub fragments: Vec<Fragment>,
     pub function_calls: BTreeMap<usize, HostFunction>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Fragment {
     Expression { expression: Expression },
     UnexpectedToken { token: Token },
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Expression {
     LiteralValue { value: f64 },
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Token {
     Identifier { name: String },
     LiteralNumber { value: f64 },
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct HostFunction {
     pub id: usize,
 }
