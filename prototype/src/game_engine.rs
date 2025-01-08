93d82506dec9f2c20b5d8c7d8120ee94c4c77e0c
Hanno Braun <hello@hannobraun.com>
2025-01-08 14:20:33 +0100

    Convert free function into constructor

--- prototype/src/game_engine.rs
+++ prototype/src/game_engine.rs
@@ -13,6 +13,70 @@ pub struct GameEngine {
     pub senders: GameEngineSenders,
 }
 
+impl GameEngine {
+    pub fn start(
+        game_output: Sender<GameOutput>,
+    ) -> anyhow::Result<GameEngine> {
+        let mut code = Code::default();
+        let mut interpreter = Interpreter::default();
+
+        editor::update(&code, &interpreter)?;
+
+        let handle_events = Actor::spawn(move |event| {
+            match event {
+                Event::EditorInput { line } => {
+                    compile(&line, &mut code);
+
+                    match interpreter.step(&code) {
+                        InterpreterState::Error => {
+                            // Not handling errors right now. Eventually, those
+                            // should be properly encoded in `Code` and
+                            // therefore visible in the editor. But in any case,
+                            // there's nothing to do here, at least for now.
+                        }
+                        InterpreterState::Finished { output } => {
+                            game_output.send(GameOutput::SubmitColor {
+                                color: [output, output, output, 1.],
+                            })?;
+                        }
+                    }
+
+                    editor::update(&code, &interpreter)?;
+                }
+                Event::GameInput(GameInput::RenderingFrame) => {
+                    // This loop is coupled to the frame rate of the renderer.
+                }
+            }
+
+            Ok(())
+        });
+
+        let events_from_editor_input = handle_events.sender.clone();
+        let handle_editor_input = Actor::spawn(move |line| {
+            events_from_editor_input.send(Event::EditorInput { line })?;
+            Ok(())
+        });
+
+        let events_from_game_input = handle_events.sender;
+        let handle_game_input = Actor::spawn(move |input| {
+            events_from_game_input.send(Event::GameInput(input))?;
+            Ok(())
+        });
+
+        let threads = GameEngineThreads {
+            handle: handle_events.handle,
+            handle_editor_input: handle_editor_input.handle,
+            handle_game_input: handle_game_input.handle,
+        };
+        let senders = GameEngineSenders {
+            editor_input: handle_editor_input.sender,
+            game_input: handle_game_input.sender,
+        };
+
+        Ok(GameEngine { threads, senders })
+    }
+}
+
 pub struct GameEngineThreads {
     handle: ThreadHandle,
     handle_editor_input: ThreadHandle,
@@ -32,66 +96,6 @@ impl GameEngineThreads {
 pub struct GameEngineSenders {
     pub editor_input: Sender<String>,
     pub game_input: Sender<GameInput>,
-}
-
-pub fn start(game_output: Sender<GameOutput>) -> anyhow::Result<GameEngine> {
-    let mut code = Code::default();
-    let mut interpreter = Interpreter::default();
-
-    editor::update(&code, &interpreter)?;
-
-    let handle_events = Actor::spawn(move |event| {
-        match event {
-            Event::EditorInput { line } => {
-                compile(&line, &mut code);
-
-                match interpreter.step(&code) {
-                    InterpreterState::Error => {
-                        // Not handling errors right now. Eventually, those
-                        // should be properly encoded in `Code` and therefore
-                        // visible in the editor. But in any case, there's
-                        // nothing to do here, at least for now.
-                    }
-                    InterpreterState::Finished { output } => {
-                        game_output.send(GameOutput::SubmitColor {
-                            color: [output, output, output, 1.],
-                        })?;
-                    }
-                }
-
-                editor::update(&code, &interpreter)?;
-            }
-            Event::GameInput(GameInput::RenderingFrame) => {
-                // This loop is coupled to the frame rate of the renderer.
-            }
-        }
-
-        Ok(())
-    });
-
-    let events_from_editor_input = handle_events.sender.clone();
-    let handle_editor_input = Actor::spawn(move |line| {
-        events_from_editor_input.send(Event::EditorInput { line })?;
-        Ok(())
-    });
-
-    let events_from_game_input = handle_events.sender;
-    let handle_game_input = Actor::spawn(move |input| {
-        events_from_game_input.send(Event::GameInput(input))?;
-        Ok(())
-    });
-
-    let threads = GameEngineThreads {
-        handle: handle_events.handle,
-        handle_editor_input: handle_editor_input.handle,
-        handle_game_input: handle_game_input.handle,
-    };
-    let senders = GameEngineSenders {
-        editor_input: handle_editor_input.sender,
-        game_input: handle_game_input.sender,
-    };
-
-    Ok(GameEngine { threads, senders })
 }
 
 enum Event {
--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -6,9 +6,11 @@ mod language;
 mod stdin;
 
 fn main() -> anyhow::Result<()> {
+    use game_engine::GameEngine;
+
     let (game_output_tx, game_output_rx) = actor::channel();
 
-    let game_engine = game_engine::start(game_output_tx)?;
+    let game_engine = GameEngine::start(game_output_tx)?;
     let mut editor = stdin::start(game_engine.senders.editor_input);
     game_io::start_and_wait(game_engine.senders.game_input, game_output_rx)?;
 
