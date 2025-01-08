5790d8765754d258a0d1b7c86578a642671e0134
Hanno Braun <hello@hannobraun.com>
2025-01-08 13:07:22 +0100

    Remove test about host functions
    I've decided I'm going to remove this feature for the time being. It
    will come back, but not having it will aid the ongoing transition to a
    functional evaluation model.

--- prototype/src/language/tests.rs
+++ prototype/src/language/tests.rs
@@ -42,30 +42,6 @@ fn code_after_expression_is_an_error() {
     assert_eq!(interpreter.step(&code), InterpreterState::Error);
 }
 
-#[test]
-fn call_to_host_function() {
-    // The host can define functions which Crosscut code can call. This should
-    // result in the interpreter notifying the host of this call, so it may
-    // handle it.
-
-    let host = Host::from_function_names(["host_fn"]);
-    let mut code = Code::default();
-    let mut interpreter = Interpreter::default();
-
-    compile("host_fn 1", &host, &mut code);
-
-    let (id, input) = loop {
-        if let InterpreterState::CallToHostFunction { id, input } =
-            interpreter.step(&code)
-        {
-            break (id, input);
-        }
-    };
-
-    assert_eq!(id, host.function_by_name("host_fn").unwrap().id);
-    assert_eq!(input, 1.);
-}
-
 fn compile(input: &str, host: &Host, code: &mut Code) {
     let mut copy_of_code = code.clone();
 
