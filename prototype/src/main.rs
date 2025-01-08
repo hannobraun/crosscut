3b31b5501f0ac236472bce5184f8da408985edfd
Hanno Braun <hello@hannobraun.com>
2025-01-08 14:01:35 +0100

    Update name of variable

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -8,12 +8,11 @@ mod stdin;
 fn main() -> anyhow::Result<()> {
     let (game_output_tx, game_output_rx) = actor::channel();
 
-    let (mut language, mut commands, mut game_input) =
-        host::start(game_output_tx)?;
+    let (mut host, mut commands, mut game_input) = host::start(game_output_tx)?;
     let mut editor = stdin::start(commands.sender);
     game_io::start_and_wait(game_input.sender, game_output_rx)?;
 
-    language.join()?;
+    host.join()?;
     commands.handle.join()?;
     game_input.handle.join()?;
     editor.join()?;
