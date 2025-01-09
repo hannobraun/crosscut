3a9ba0e8f2f2fbf4a79494914091d0d3e66d9031
Hanno Braun <hello@hannobraun.com>
2025-01-09 13:33:04 +0100

    Simplify

--- prototype/src/language/code/hash.rs
+++ prototype/src/language/code/hash.rs
@@ -1,16 +1,4 @@
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Hash {
     inner: [u8; 32],
-}
-
-impl Ord for Hash {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.inner.cmp(&other.inner)
-    }
-}
-
-impl PartialOrd for Hash {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
 }
