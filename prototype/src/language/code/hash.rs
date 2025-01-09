769db892f379af7ff88c5634bc510fd00b46c85e
Hanno Braun <hello@hannobraun.com>
2025-01-09 13:31:16 +0100

    Implement `Ord` for `Hash`

--- prototype/src/language/code/hash.rs
+++ prototype/src/language/code/hash.rs
@@ -2,3 +2,15 @@
 pub struct Hash {
     inner: blake3::Hash,
 }
+
+impl Ord for Hash {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.inner.as_bytes().cmp(other.inner.as_bytes())
+    }
+}
+
+impl PartialOrd for Hash {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
