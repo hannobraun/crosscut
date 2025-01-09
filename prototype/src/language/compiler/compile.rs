dc212005f711e084dff3893a0e18b483e407caaf
Hanno Braun <hello@hannobraun.com>
2025-01-09 01:49:50 +0100

    Prepare to expand `compiler` module

---
+++ prototype/src/language/compiler/compile.rs
@@ -0,0 +1,35 @@
+use crate::language::code::{Code, Expression, Fragment, Token};
+
+pub fn compile(input: &str, code: &mut Code) {
+    for token in tokenize(input) {
+        let fragment = match token {
+            Token::Identifier { name } => Fragment::UnexpectedToken {
+                token: Token::Identifier { name },
+            },
+            Token::LiteralNumber { value } => {
+                if code.is_complete() {
+                    Fragment::UnexpectedToken {
+                        token: Token::LiteralNumber { value },
+                    }
+                } else {
+                    Fragment::Expression {
+                        expression: Expression::LiteralValue { value },
+                    }
+                }
+            }
+        };
+
+        code.fragments.push(fragment);
+    }
+}
+
+fn tokenize(input: &str) -> impl Iterator<Item = Token> + '_ {
+    input
+        .split_whitespace()
+        .map(|token| match token.parse::<f64>() {
+            Ok(value) => Token::LiteralNumber { value },
+            Err(_) => Token::Identifier {
+                name: token.to_string(),
+            },
+        })
+}
