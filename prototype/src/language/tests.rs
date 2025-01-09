d202131420f38c587c79aca49f11ac9c6148db72
Hanno Braun <hello@hannobraun.com>
2025-01-09 01:52:21 +0100

    Fix formatting in comment

--- prototype/src/language/tests.rs
+++ prototype/src/language/tests.rs
@@ -27,8 +27,8 @@ fn code_after_expression_is_an_error() {
     // An expression returns a value. That value can be returned when the
     // program finishes, or it can be used as the argument of a function call.
     //
-    // Either way,any code that comes after an expression makes no sense, and is
-    // an error.
+    // Either way, any code that comes after an expression makes no sense, and
+    // is an error.
 
     let mut code = Code::default();
     let mut interpreter = Interpreter::default();
