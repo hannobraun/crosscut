21a16f3ef71ea0e7f964630a5479692892b809c4
Hanno Braun <hello@hannobraun.com>
2025-01-10 10:31:33 +0100

    Make name of struct field more explicit

--- prototype/src/language/code/fragments.rs
+++ prototype/src/language/code/fragments.rs
@@ -8,11 +8,11 @@ pub type Fragments = BTreeMap<Id, Fragment>;
     Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable,
 )]
 pub struct Id {
-    value: [u8; 32],
+    hash: [u8; 32],
 }
 impl Id {
     pub fn of(fragment: &Fragment) -> Self {
         let value = udigest::hash::<blake3::Hasher>(fragment).into();
-        Self { value }
+        Self { hash: value }
     }
 }
