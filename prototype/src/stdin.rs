dd44bde80895a84c21d7d5a643f083890cfc43e6
Hanno Braun <hello@hannobraun.com>
2025-01-12 15:10:55 +0100

    Fix hanging thread on shutdown

--- prototype/src/stdin.rs
+++ prototype/src/stdin.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crossterm::event::{self, Event, KeyCode};
 
 use crate::thread::{self, Sender, ThreadHandle};
@@ -6,6 +8,18 @@ pub fn start(editor_input: Sender<Option<String>>) -> ThreadHandle {
     let mut line = String::new();
 
     thread::spawn(move || {
+        let timeout = Duration::from_millis(500);
+        let event_ready = event::poll(timeout)?;
+
+        if !event_ready {
+            // We must send on the channel from time to time, to make sure we
+            // learn if the other thread has shut down. Otherwise, this thread
+            // will hang forever, blocking on input, preventing the application
+            // from shutting down.
+            editor_input.send(None)?;
+            return Ok(());
+        }
+
         let event = event::read()?;
 
         let Event::Key(key_event) = event else {
