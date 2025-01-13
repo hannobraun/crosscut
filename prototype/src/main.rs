11e454208be49d6e39e099f897f7f5e1150343db
Hanno Braun <hello@hannobraun.com>
2025-01-13 14:08:17 +0100

    Prepare to consolidate all I/O in single module

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -5,7 +5,7 @@
 
 mod editor;
 mod game_engine;
-mod game_io;
+mod io;
 mod language;
 mod stdin;
 mod thread;
@@ -17,7 +17,7 @@ fn main() -> anyhow::Result<()> {
 
     let game_engine = GameEngine::start(game_output_tx)?;
     let editor = stdin::start(game_engine.editor_input);
-    game_io::start_and_wait(game_engine.game_input, game_output_rx)?;
+    io::game_io::start_and_wait(game_engine.game_input, game_output_rx)?;
 
     game_engine.handle.join()?;
     editor.join()?;
