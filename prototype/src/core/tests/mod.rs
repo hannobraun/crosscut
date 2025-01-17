2349aa752582b7341f4e7de8d32c34251a3885c6
Hanno Braun <hello@hannobraun.com>
2025-01-17 13:00:47 +0100

    Move host function tests to dedicated module
    There are only two of them, but they both require a special setup. I
    think it's justified.

--- prototype/src/core/tests/mod.rs
+++ prototype/src/core/tests/mod.rs
@@ -1,1 +1,2 @@
+mod host_functions;
 mod suite;
--- prototype/src/core/tests/suite.rs
+++ prototype/src/core/tests/suite.rs
@@ -2,9 +2,8 @@ use pretty_assertions::assert_eq;
 
 use crate::core::{
     self,
-    code::Code,
     host::Host,
-    interpreter::{Interpreter, StepResult, Value},
+    interpreter::{StepResult, Value},
 };
 
 #[test]
@@ -45,65 +44,4 @@ fn code_after_expression_is_an_error() {
         },
     );
     assert_eq!(core.interpreter.step(&core.code), StepResult::Error);
-}
-
-#[test]
-fn call_to_host_function() {
-    // The host can define functions. Those functions take one argument, return
-    // one value, and can be called from Crosscut code.
-
-    let output = compile_and_run("half 64");
-    assert_eq!(output, Value::Integer { value: 32 });
-}
-
-#[test]
-fn nested_calls_to_host_function() {
-    // It is possible use a function call as the argument of another function
-    // call.
-
-    let output = compile_and_run("half half 64");
-    assert_eq!(output, Value::Integer { value: 16 });
-}
-
-fn compile_and_run(input_code: &str) -> Value {
-    let host = Host::from_functions(["half"]);
-    let mut core = core::Instance::new();
-
-    core.edit(input_code, &host);
-
-    // The editor already resets the interpreter, but only if it's not running.
-    // The tests that use this function provide multiple tokens. The editor
-    // resets the interpreter after the first token, and then it _is_ running,
-    // but never actually stepped until all code has been provided.
-    //
-    // As a result, the interpreter then tries to run the fragment resulting
-    // from that first token, which doesn't have arguments yet, resulting in an
-    // error.
-    //
-    // So we need to reset again manually, once all code has been provided, so
-    // the interpreter actually runs the most recent version of the code.
-    core.interpreter.reset(&core.code);
-
-    run(&core.code, &mut core.interpreter)
-}
-
-fn run(code: &Code, interpreter: &mut Interpreter) -> Value {
-    loop {
-        match interpreter.step(code) {
-            StepResult::CallToHostFunction { id, input, output } => {
-                assert_eq!(id, 0);
-
-                let Value::Integer { value: input } = input;
-                let Value::Integer { value: output } = output;
-
-                *output = input / 2;
-            }
-            StepResult::Finished { output } => {
-                break output;
-            }
-            state => {
-                panic!("Unexpected state: {state:#?}");
-            }
-        }
-    }
 }
