237fd21daaee5cec1d539ac4ce9e6333a217d9ae
Hanno Braun <hello@hannobraun.com>
2025-01-19 18:42:08 +0100

    Simplify

--- prototype/src/lang/editor.rs
+++ prototype/src/lang/editor.rs
@@ -101,7 +101,7 @@ impl Editor {
             InputEvent::Backspace => {
                 self.input.remove_left();
             }
-            InputEvent::Enter => match &mut self.mode {
+            InputEvent::Enter => match &self.mode {
                 EditorMode::Command => {
                     self.process_command(code, interpreter);
                     self.input.clear();
