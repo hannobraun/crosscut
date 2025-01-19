5d04d73b00fb14b0a7378a21b94ad3b210ec407b
Hanno Braun <hello@hannobraun.com>
2025-01-19 18:16:36 +0100

    Remove redundant `pub`

--- prototype/src/lang/code/replacements.rs
+++ prototype/src/lang/code/replacements.rs
@@ -4,7 +4,7 @@ use super::FragmentId;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Replacements {
-    pub inner: BTreeMap<FragmentId, FragmentId>,
+    inner: BTreeMap<FragmentId, FragmentId>,
 }
 
 impl Replacements {
