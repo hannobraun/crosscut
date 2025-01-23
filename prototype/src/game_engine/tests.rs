483060a342f06829197bb393b42145f60ac2db80
Hanno Braun <hello@hannobraun.com>
2025-01-23 14:57:48 +0100

    Port test to game engine

--- prototype/src/game_engine/tests.rs
+++ prototype/src/game_engine/tests.rs
@@ -1,6 +1,6 @@
 use crate::{
     game_engine::{GameEngine, GameOutput},
-    lang::{self, editor::InputEvent, host::Host, interpreter::Value},
+    lang::editor::InputEvent,
 };
 
 #[test]
@@ -35,20 +35,25 @@ fn abort_command_without_executing_on_escape_key() {
     // escape key. This should return us to edit mode, without a command being
     // executed.
 
-    let host = Host::empty();
-    let mut lang = lang::Instance::new();
+    let mut game_engine = GameEngine::without_editor_ui();
 
     // Start editing the code.
-    lang.on_code("1", &host);
+    game_engine.on_code("12");
 
     // Enter a command, but abort it.
-    lang.on_event(InputEvent::Escape, &host); // enter command mode
-    lang.on_input("clear", &host);
-    lang.on_event(InputEvent::Escape, &host);
+    game_engine.on_editor_input(InputEvent::Escape).unwrap();
+    game_engine.on_input("clear");
+    game_engine.on_editor_input(InputEvent::Escape).unwrap();
+
+    // Drain output. We're only interested in the result of the next change.
+    let _ = game_engine.game_output();
 
     // Continue editing. If the previous command was executed, this won't have
     // the desired result.
-    lang.on_code("2", &host);
+    game_engine.on_code("7");
 
-    assert_eq!(lang.run_until_finished(), Value::Integer { value: 12 });
+    let GameOutput::SubmitColor { color } =
+        game_engine.game_output().next().unwrap();
+    let c = 127. / 255.;
+    assert_eq!(color.as_slice(), [c, c, c, 1.0].as_slice());
 }
