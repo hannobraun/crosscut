e9588ea0737bddeb604ca9971ea290b923ef3c1b
Hanno Braun <hello@hannobraun.com>
2025-01-24 12:45:38 +0100

    Update name of module

--- prototype/src/lang/code/mod.rs
+++ prototype/src/lang/code/mod.rs
@@ -1,5 +1,5 @@
 mod body;
-mod code;
+mod codebase;
 mod errors;
 mod fragments;
 mod location;
@@ -7,7 +7,7 @@ mod replacements;
 
 pub use self::{
     body::Body,
-    code::{Codebase, Expression, FunctionCallTarget, Literal},
+    codebase::{Codebase, Expression, FunctionCallTarget, Literal},
     errors::CodeError,
     fragments::{Fragment, FragmentError, FragmentId, FragmentKind, Fragments},
     location::{Located, Location},
