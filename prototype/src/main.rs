ef579a97cc77aa9bf96bab4fab3a250d48d489a8
Hanno Braun <hello@hannobraun.com>
2025-01-26 16:55:55 +0100

    Add placeholder for `game_engine` module

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,3 +1,8 @@
+mod game_engine;
+
 fn main() {
-    println!("Hello, world!");
+    dbg!(game_engine::GameInput::RenderingFrame);
+    let game_engine::GameOutput::SubmitColor { color } =
+        game_engine::GameOutput::SubmitColor { color: [1.; 4] };
+    dbg!(color);
 }
