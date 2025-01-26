ab7b0d25c9ba13bf2249178f8e7cee982f69643c
Hanno Braun <hello@hannobraun.com>
2025-01-26 22:11:37 +0100

    Add comment

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -4,6 +4,9 @@ mod threads;
 
 fn main() -> anyhow::Result<()> {
     let threads = threads::start()?;
+
+    // This call is going to block until the user requests a shutdown via the
+    // game I/O, or any of the other threads shut down.
     io::game_engine::start_and_wait(threads.game_input, threads.game_output)?;
 
     for handle in threads.handles {
