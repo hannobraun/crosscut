a06a4ee25f7d8de58c8deb60f3d227965f30d0e6
Hanno Braun <hello@hannobraun.com>
2025-01-26 22:11:43 +0100

    Add comment

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -9,6 +9,9 @@ fn main() -> anyhow::Result<()> {
     // game I/O, or any of the other threads shut down.
     io::game_engine::start_and_wait(threads.game_input, threads.game_output)?;
 
+    // At this point, the shutdown should be in progress, and none of these
+    // calls block for long, if at all. The purpose of still joining all threads
+    // is just to get any error they might have produced.
     for handle in threads.handles {
         handle.join()?;
     }
