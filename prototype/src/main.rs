bb8b48515a3c68ecf97a09d3372e0e2b6bc5b3b5
Hanno Braun <hello@hannobraun.com>
2025-01-26 17:07:11 +0100

    Import parts of `threads` module

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,10 +1,19 @@
 mod game_engine;
+mod threads;
 
 fn main() -> anyhow::Result<()> {
-    dbg!(game_engine::GameInput::RenderingFrame);
-    let game_engine::GameOutput::SubmitColor { color } =
-        game_engine::GameOutput::SubmitColor { color: [1.; 4] };
-    dbg!(color);
+    let (game_input_tx, game_input_rx) = threads::channel();
+    let (game_output_tx, game_output_rx) = threads::channel();
+
+    game_input_tx.send(game_engine::GameInput::RenderingFrame)?;
+    dbg!(game_input_rx.try_recv())?;
+
+    game_output_tx
+        .send(game_engine::GameOutput::SubmitColor { color: [1.; 4] })?;
+    if let Some(game_output) = game_output_rx.try_recv()? {
+        let game_engine::GameOutput::SubmitColor { color } = game_output;
+        dbg!(color);
+    }
 
     Ok(())
 }
---
+++ prototype/src/threads.rs
@@ -0,0 +1,45 @@
+use crossbeam_channel::{SendError, TryRecvError};
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let (sender, receiver) = crossbeam_channel::unbounded();
+
+    (Sender { inner: sender }, Receiver { inner: receiver })
+}
+
+pub struct Sender<T> {
+    inner: crossbeam_channel::Sender<T>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(&self, value: T) -> Result<(), ChannelDisconnected> {
+        self.inner
+            .send(value)
+            .map_err(|SendError(_)| ChannelDisconnected)
+    }
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+pub struct Receiver<T> {
+    inner: crossbeam_channel::Receiver<T>,
+}
+
+impl<T> Receiver<T> {
+    pub fn try_recv(&self) -> Result<Option<T>, ChannelDisconnected> {
+        match self.inner.try_recv() {
+            Ok(message) => Ok(Some(message)),
+            Err(TryRecvError::Empty) => Ok(None),
+            Err(TryRecvError::Disconnected) => Err(ChannelDisconnected),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+#[error("Channel disconnected")]
+pub struct ChannelDisconnected;
