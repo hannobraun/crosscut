f3e99f5e97f71d3505714329124d685a3fe75f67
Hanno Braun <hello@hannobraun.com>
2025-01-26 22:12:39 +0100

    Reduce diff with new prototype

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ mod threads;
 fn main() -> anyhow::Result<()> {
     let threads = threads::start()?;
 
+    // This call is going to block until the user requests a shutdown via the
+    // game I/O, or any of the other threads shut down.
     io::game_engine::start_and_wait(threads.game_input, threads.game_output)?;
-    // At this point, we're blocking until any of the threads shut down. There's
-    // nothing to join yet, because the game engine I/O is using `winit`
-    // internally, which requires its own special handling.
 
+    // At this point, the shutdown should be in progress, and none of these
+    // calls block for long, if at all. The purpose of still joining all threads
+    // is just to get any error they might have produced.
     for handle in threads.handles {
         handle.join()?;
     }
