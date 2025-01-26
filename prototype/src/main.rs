1a9f84333a3ac42395f9495b874cb2a3af69d07b
Hanno Braun <hello@hannobraun.com>
2025-01-26 17:16:15 +0100

    Import more of `threads`

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -3,14 +3,12 @@ mod io;
 mod threads;
 
 fn main() -> anyhow::Result<()> {
-    let (game_input_tx, game_input_rx) = threads::channel();
-    let (game_output_tx, game_output_rx) = threads::channel();
+    let threads = threads::start()?;
+    io::game_engine::start_and_wait(threads.game_input, threads.game_output)?;
 
-    dbg!(game_input_rx.try_recv())?;
-    game_output_tx
-        .send(game_engine::GameOutput::SubmitColor { color: [1.; 4] })?;
-
-    io::game_engine::start_and_wait(game_input_tx, game_output_rx)?;
+    for handle in threads.handles {
+        handle.join()?;
+    }
 
     Ok(())
 }
--- prototype/src/threads.rs
+++ prototype/src/threads.rs
@@ -1,9 +1,64 @@
-use crossbeam_channel::{SendError, TryRecvError};
+use std::{
+    io,
+    ops::ControlFlow,
+    panic,
+    thread::{self, JoinHandle},
+};
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let (sender, receiver) = crossbeam_channel::unbounded();
+use crossbeam_channel::{select, SendError, TryRecvError};
 
-    (Sender { inner: sender }, Receiver { inner: receiver })
+use crate::game_engine::{GameInput, GameOutput};
+
+pub fn start() -> anyhow::Result<Threads> {
+    let (game_input_tx, game_input_rx) = channel();
+    let (game_output_tx, game_output_rx) = channel();
+
+    let game_engine = spawn(move || loop {
+        let event = select! {
+            recv(game_input_rx.inner) -> result => {
+                result
+            }
+        };
+        let Ok(event) = event else {
+            return Err(ChannelDisconnected.into());
+        };
+
+        dbg!(event);
+
+        game_output_tx.send(GameOutput::SubmitColor { color: [1.; 4] })?;
+    });
+
+    Ok(Threads {
+        handles: [game_engine],
+        game_input: game_input_tx,
+        game_output: game_output_rx,
+    })
+}
+
+pub struct Threads {
+    pub handles: [ThreadHandle; 1],
+    pub game_input: Sender<GameInput>,
+    pub game_output: Receiver<GameOutput>,
+}
+
+#[derive(Debug)]
+pub struct ThreadHandle {
+    inner: JoinHandle<anyhow::Result<()>>,
+}
+
+impl ThreadHandle {
+    fn new(handle: JoinHandle<anyhow::Result<()>>) -> Self {
+        Self { inner: handle }
+    }
+
+    pub fn join(self) -> anyhow::Result<()> {
+        match self.inner.join() {
+            Ok(result) => result,
+            Err(payload) => {
+                panic::resume_unwind(payload);
+            }
+        }
+    }
 }
 
 pub struct Sender<T> {
@@ -41,5 +96,63 @@ impl<T> Receiver<T> {
 }
 
 #[derive(Debug, thiserror::Error)]
+enum Error {
+    /// # Channel is disconnected
+    ///
+    /// This should only happen, if another thread has shut down. Within the
+    /// scope of this application, this means that the overall system either
+    /// already is shutting down, or should be going into shutdown.
+    #[error(transparent)]
+    ChannelDisconnected {
+        #[from]
+        err: ChannelDisconnected,
+    },
+
+    #[error(transparent)]
+    Other {
+        #[from]
+        err: anyhow::Error,
+    },
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::Other { err: err.into() }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
 #[error("Channel disconnected")]
 pub struct ChannelDisconnected;
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let (sender, receiver) = crossbeam_channel::unbounded();
+
+    (Sender { inner: sender }, Receiver { inner: receiver })
+}
+
+fn spawn<F>(mut f: F) -> ThreadHandle
+where
+    F: FnMut() -> Result<ControlFlow<()>, Error> + Send + 'static,
+{
+    let handle = thread::spawn(move || {
+        loop {
+            match f() {
+                Ok(ControlFlow::Continue(())) => {}
+                Ok(ControlFlow::Break(())) => {
+                    break;
+                }
+                Err(Error::ChannelDisconnected { .. }) => {
+                    break;
+                }
+                Err(Error::Other { err }) => {
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(())
+    });
+
+    ThreadHandle::new(handle)
+}
