e141770d320b5c54ddce9d3bd19bb31b2a85c2e9
Hanno Braun <hello@hannobraun.com>
2025-01-27 13:09:44 +0100

    Add `EditorInput`

--- prototype/src/language/editor.rs
+++ prototype/src/language/editor.rs
@@ -1,19 +1,38 @@
 use super::code::Codebase;
 
 #[derive(Debug)]
-pub struct Editor {}
+pub struct Editor {
+    input: EditorInput,
+}
 
 impl Editor {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            input: EditorInput::new(String::new()),
+        }
     }
 
     pub fn on_input(&mut self, event: EditorInputEvent, _: &mut Codebase) {
         if let EditorInputEvent::Character { ch } = event {
-            dbg!(ch);
+            self.input.insert(ch);
         } else {
             dbg!(event);
         }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct EditorInput {
+    buffer: String,
+}
+
+impl EditorInput {
+    pub fn new(buffer: String) -> Self {
+        Self { buffer }
+    }
+
+    pub fn insert(&mut self, ch: char) {
+        self.buffer.push(ch);
     }
 }
 
