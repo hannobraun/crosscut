4b921f10c2b5868b63cac6ad373393c9704418fd
Hanno Braun <hello@hannobraun.com>
2025-01-27 15:32:59 +0100

    Document test

--- prototype/src/language/tests/expressions.rs
+++ prototype/src/language/tests/expressions.rs
@@ -2,6 +2,9 @@ use crate::language::instance::Language;
 
 #[test]
 fn evaluate_integer() {
+    // A number literal is a function that takes `None` and returns the number
+    // it represents.
+
     let mut language = Language::new();
 
     language.enter_code("127");
