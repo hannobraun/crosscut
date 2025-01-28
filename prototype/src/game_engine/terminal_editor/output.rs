e6d232bd73a252bfb730f043b6be99f6b29de425
Hanno Braun <hello@hannobraun.com>
2025-01-28 12:11:21 +0100

    Render current code in editor

--- prototype/src/game_engine/terminal_editor/output.rs
+++ prototype/src/game_engine/terminal_editor/output.rs
@@ -1,6 +1,6 @@
 use crate::{
     io::terminal_editor::output::EditorOutputAdapter,
-    language::instance::Language,
+    language::{code::Codebase, instance::Language},
 };
 
 #[derive(Debug)]
@@ -16,11 +16,32 @@ where
         Self { adapter }
     }
 
-    pub fn render(&mut self, _: &Language) -> anyhow::Result<()> {
+    pub fn render(&mut self, language: &Language) -> anyhow::Result<()> {
+        let mut context = RenderContext {
+            codebase: &language.codebase,
+        };
+
         self.adapter.clear()?;
+
+        render_code(&mut self.adapter, &mut context)?;
 
         self.adapter.flush()?;
 
         Ok(())
     }
+}
+
+fn render_code<A: EditorOutputAdapter>(
+    adapter: &mut A,
+    context: &mut RenderContext,
+) -> anyhow::Result<()> {
+    if let Some(value) = context.codebase.value {
+        write!(adapter, "{value}")?;
+    }
+
+    Ok(())
+}
+
+struct RenderContext<'r> {
+    codebase: &'r Codebase,
 }
