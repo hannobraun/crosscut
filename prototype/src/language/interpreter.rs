69311a84326878a77cd0dc0606f2c14326d0848d
Hanno Braun <hello@hannobraun.com>
2025-01-29 13:43:41 +0100

    Prepare to support multiple tokens

--- prototype/src/language/interpreter.rs
+++ prototype/src/language/interpreter.rs
@@ -1,18 +1,24 @@
 use super::code::{Codebase, Expression, Node};
 
 #[derive(Debug)]
-pub struct Interpreter {}
+pub struct Interpreter {
+    current_value: Value,
+}
 
 impl Interpreter {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            current_value: Value::None,
+        }
     }
 
     pub fn step(&mut self, codebase: &Codebase) -> StepResult {
         let Node::Expression {
             expression: Expression::LiteralValue { value: output },
         } = codebase.nodes.first().cloned().unwrap_or(Node::Expression {
-            expression: Expression::LiteralValue { value: Value::None },
+            expression: Expression::LiteralValue {
+                value: self.current_value,
+            },
         });
         StepResult::Finished { output }
     }
