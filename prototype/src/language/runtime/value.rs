fb3ae563562be12f9a20bfbaecaf2752893d9dbd
Hanno Braun <hello@hannobraun.com>
2025-01-30 22:11:39 +0100

    Implement `Display` for `Value`

--- prototype/src/language/runtime/value.rs
+++ prototype/src/language/runtime/value.rs
@@ -1,5 +1,16 @@
+use std::fmt;
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Value {
     None,
     Integer { value: i32 },
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Value::None => write!(f, "none"),
+            Value::Integer { value } => write!(f, "{value}"),
+        }
+    }
 }
