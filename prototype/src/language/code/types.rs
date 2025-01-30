76c7120a63d9c911a677185c318d73edd5aca6b5
Hanno Braun <hello@hannobraun.com>
2025-01-30 22:03:28 +0100

    Implement `Display` for `Type`

--- prototype/src/language/code/types.rs
+++ prototype/src/language/code/types.rs
@@ -1,4 +1,16 @@
+use std::fmt;
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Type {
     Integer,
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let text = match self {
+            Type::Integer => "Integer",
+        };
+
+        write!(f, "{text}")
+    }
 }
