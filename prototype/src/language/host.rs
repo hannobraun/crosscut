97f0c07d8150e8cc10346dd0877f46d55529e32d
Hanno Braun <hello@hannobraun.com>
2025-01-30 01:04:25 +0100

    Prepare to support host functions

--- prototype/src/language/host.rs
+++ prototype/src/language/host.rs
@@ -1,20 +1,28 @@
+use std::collections::BTreeMap;
+
 #[derive(Debug)]
-pub struct Host {}
+pub struct Host {
+    function_ids_by_name: BTreeMap<String, u32>,
+    function_names_by_id: BTreeMap<u32, String>,
+}
 
 impl Host {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            function_ids_by_name: BTreeMap::new(),
+            function_names_by_id: BTreeMap::new(),
+        }
     }
 
     pub fn function_id_by_name(&self, _name: &str) -> Option<u32> {
-        // This is a placeholder while host functions are still being
-        // implemented.
-        None
+        self.function_ids_by_name.get(_name).copied()
     }
 
     pub fn function_name_by_id(&self, _id: &u32) -> &str {
-        // This is a placeholder while host functions are still being
-        // implemented.
-        ""
+        let Some(name) = self.function_names_by_id.get(_id) else {
+            panic!("Expected function ID `{_id}` to be valid.");
+        };
+
+        name
     }
 }
