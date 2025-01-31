c430155c6969712af116aef906d997cbfb9976fd
Hanno Braun <hello@hannobraun.com>
2025-01-31 13:36:09 +0100

    Add support for formatting `Expression`

--- prototype/src/language/code/expressions.rs
+++ prototype/src/language/code/expressions.rs
@@ -1,7 +1,40 @@
+use std::fmt;
+
+use crate::language::host::Host;
+
 use super::IntrinsicFunction;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Expression {
     HostFunction { id: u32 },
     IntrinsicFunction { function: IntrinsicFunction },
+}
+
+impl Expression {
+    #[allow(unused)] // code using this is being worked on
+    pub fn display<'r>(&'r self, host: &'r Host) -> ExpressionDisplay<'r> {
+        ExpressionDisplay {
+            expression: self,
+            host,
+        }
+    }
+}
+
+pub struct ExpressionDisplay<'r> {
+    expression: &'r Expression,
+    host: &'r Host,
+}
+
+impl fmt::Display for ExpressionDisplay<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.expression {
+            Expression::HostFunction { id } => {
+                let name = self.host.function_name_by_id(id);
+                write!(f, "{name}")
+            }
+            Expression::IntrinsicFunction { function } => {
+                write!(f, "{function}")
+            }
+        }
+    }
 }
