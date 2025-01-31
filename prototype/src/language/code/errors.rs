e1d78f0b82845ebf8d98a51902e8a05b339e5531
Hanno Braun <hello@hannobraun.com>
2025-01-31 12:29:52 +0100

    Prepare for follow-on change

--- prototype/src/language/code/errors.rs
+++ prototype/src/language/code/errors.rs
@@ -9,8 +9,10 @@ impl fmt::Display for CodeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             CodeError::UnresolvedIdentifier => {
-                write!(f, "unresolved identifier")
+                write!(f, "unresolved identifier")?;
             }
         }
+
+        Ok(())
     }
 }
