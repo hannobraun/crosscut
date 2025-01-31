2ed86ab70f1c343d617c7c9599186f05bc87b22b
Hanno Braun <hello@hannobraun.com>
2025-01-31 14:35:05 +0100

    Prepare for follow-on change

--- prototype/src/language/code/codebase.rs
+++ prototype/src/language/code/codebase.rs
@@ -11,7 +11,7 @@ pub struct Codebase {
 impl Codebase {
     pub fn new() -> Self {
         Self {
-            nodes: Vec::new(),
+            nodes: vec![],
             errors: BTreeMap::new(),
         }
     }
