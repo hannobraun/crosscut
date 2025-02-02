db8316433f307d37f1ecb698882b55026256d265
Hanno Braun <hello@hannobraun.com>
2025-02-02 13:53:06 +0100

    Prepare to remove empty syntax node to the left

--- prototype/src/language/editor/input_buffer.rs
+++ prototype/src/language/editor/input_buffer.rs
@@ -113,10 +113,12 @@ impl EditorInputBuffer {
         None
     }
 
-    fn remove_left(&mut self) {
+    fn remove_left(&mut self) -> Option<UpdateAction> {
         if self.move_cursor_left().is_none() {
             self.buffer.remove(self.cursor);
         }
+
+        None
     }
 
     fn remove_right(&mut self) {
