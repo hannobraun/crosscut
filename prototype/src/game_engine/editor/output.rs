19689a7c0c1427bcb01ea49763f98e7c259ede58
Hanno Braun <hello@hannobraun.com>
2025-02-03 13:51:43 +0100

    Update name of variable

--- prototype/src/game_engine/editor/output.rs
+++ prototype/src/game_engine/editor/output.rs
@@ -93,11 +93,11 @@ fn render_interpreter_state<A: EditorOutputAdapter>(
                     Ok(())
                 })?;
             }
-            InterpreterState::Error { path: location } => {
+            InterpreterState::Error { path } => {
                 // While we have a dynamic type system, it's possible that an
                 // error is only known at runtime. In that case, we'll get
                 // `None` here.
-                let maybe_error = context.codebase.error_at(&location);
+                let maybe_error = context.codebase.error_at(&path);
 
                 adapter.color(ERROR_COLOR, |adapter| {
                     write!(adapter, "Error")?;
