13e5d3859bc9521eb410b4043e8dfea1ce81f71a
Hanno Braun <hello@hannobraun.com>
2025-02-03 14:13:52 +0100

    Sort types by order of use

--- prototype/src/language/code/nodes.rs
+++ prototype/src/language/code/nodes.rs
@@ -38,6 +38,43 @@ impl Nodes {
     }
 }
 
+/// The hash of a syntax node
+///
+/// The purpose of this type is to serve as a building block for identifying
+/// syntax nodes in a unique and versioned manner. But it's important to
+/// understand that it is not more than a building block. By itself, a hash
+/// can't be unique.
+///
+/// A hash derives from a syntax node's contents, which include its children.
+/// This is not unique, because the syntax tree can contain identical sub-trees
+/// in various places, each with a root node that would have identical hashes.
+///
+/// To guarantee uniqueness, we also need the position of the node within the
+/// syntax tree, which includes the parent. But a node's parent already includes
+/// its own children, which would cause a circular dependency when computing the
+/// hash.
+///
+/// To solve this issue, we have [`NodePath`], which after the hashes have been
+/// constructed bottom-up, is constructed top-down, from the root of the syntax
+/// tree.
+///
+/// ## Implementation Note
+///
+/// Not everything documented here, especially the relation to [`NodePath`], is
+/// implemented yet. I wanted to paint a complete picture of how this fits into
+/// my current plans, even though those are not fully realized yet.
+#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable)]
+pub struct NodeHash {
+    hash: [u8; 32],
+}
+
+impl fmt::Debug for NodeHash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", BASE64_STANDARD_NO_PAD.encode(self.hash))?;
+        Ok(())
+    }
+}
+
 #[derive(Clone, Debug, Eq, PartialEq, udigest::Digestable)]
 pub struct Node {
     pub kind: NodeKind,
@@ -94,43 +131,6 @@ impl fmt::Display for NodeDisplay<'_> {
                 write!(f, "{name}")
             }
         }
-    }
-}
-
-/// The hash of a syntax node
-///
-/// The purpose of this type is to serve as a building block for identifying
-/// syntax nodes in a unique and versioned manner. But it's important to
-/// understand that it is not more than a building block. By itself, a hash
-/// can't be unique.
-///
-/// A hash derives from a syntax node's contents, which include its children.
-/// This is not unique, because the syntax tree can contain identical sub-trees
-/// in various places, each with a root node that would have identical hashes.
-///
-/// To guarantee uniqueness, we also need the position of the node within the
-/// syntax tree, which includes the parent. But a node's parent already includes
-/// its own children, which would cause a circular dependency when computing the
-/// hash.
-///
-/// To solve this issue, we have [`NodePath`], which after the hashes have been
-/// constructed bottom-up, is constructed top-down, from the root of the syntax
-/// tree.
-///
-/// ## Implementation Note
-///
-/// Not everything documented here, especially the relation to [`NodePath`], is
-/// implemented yet. I wanted to paint a complete picture of how this fits into
-/// my current plans, even though those are not fully realized yet.
-#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable)]
-pub struct NodeHash {
-    hash: [u8; 32],
-}
-
-impl fmt::Debug for NodeHash {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", BASE64_STANDARD_NO_PAD.encode(self.hash))?;
-        Ok(())
     }
 }
 
