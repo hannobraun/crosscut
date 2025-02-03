c14d24486d97f1be95a3bd49101eec2904af38f1
Hanno Braun <hello@hannobraun.com>
2025-02-03 11:10:15 +0100

    Prepare to consolidate redundant code

--- prototype/src/language/compiler.rs
+++ prototype/src/language/compiler.rs
@@ -16,35 +16,44 @@ pub fn compile(
     let node = if token.is_empty() {
         Node::Empty
     } else {
-        let host_function = host.function_id_by_name(token);
-        let intrinsic_function = IntrinsicFunction::resolve(token);
-
-        match (host_function, intrinsic_function) {
-            (Some(id), None) => Node::Expression {
-                expression: Expression::HostFunction { id },
-            },
-            (None, Some(function)) => Node::Expression {
-                expression: Expression::IntrinsicFunction { function },
-            },
-            (None, None) => {
-                let candidates = Vec::new();
-                emit_unresolved_identifier_error(
-                    token, location, candidates, codebase,
-                )
-            }
-            (Some(id), Some(function)) => {
-                let candidates = vec![
-                    Expression::HostFunction { id },
-                    Expression::IntrinsicFunction { function },
-                ];
-                emit_unresolved_identifier_error(
-                    token, location, candidates, codebase,
-                )
-            }
-        }
+        resolve_function(token, location, host, codebase)
     };
 
     codebase.replace_node(&location, node);
+}
+
+fn resolve_function(
+    token: &str,
+    location: Location,
+    host: &Host,
+    codebase: &mut Codebase,
+) -> Node {
+    let host_function = host.function_id_by_name(token);
+    let intrinsic_function = IntrinsicFunction::resolve(token);
+
+    match (host_function, intrinsic_function) {
+        (Some(id), None) => Node::Expression {
+            expression: Expression::HostFunction { id },
+        },
+        (None, Some(function)) => Node::Expression {
+            expression: Expression::IntrinsicFunction { function },
+        },
+        (None, None) => {
+            let candidates = Vec::new();
+            emit_unresolved_identifier_error(
+                token, location, candidates, codebase,
+            )
+        }
+        (Some(id), Some(function)) => {
+            let candidates = vec![
+                Expression::HostFunction { id },
+                Expression::IntrinsicFunction { function },
+            ];
+            emit_unresolved_identifier_error(
+                token, location, candidates, codebase,
+            )
+        }
+    }
 }
 
 fn emit_unresolved_identifier_error(
