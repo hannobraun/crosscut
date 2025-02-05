d194b47b075f94980709183da8ddfbcdadea7c38
Hanno Braun <hello@hannobraun.com>
2025-02-05 17:37:14 +0100

    Prepare for follow-on change

--- prototype/src/language/code/tree.rs
+++ prototype/src/language/code/tree.rs
@@ -43,7 +43,7 @@ impl SyntaxTree {
     ) -> Option<NodePath> {
         let mut leaf_to_root = self.leaf_to_root(nodes);
 
-        leaf_to_root.find_map(|located_node| {
+        leaf_to_root.by_ref().find_map(|located_node| {
             (located_node.node.child.as_ref() == Some(node))
                 .then_some(located_node.path)
         })
