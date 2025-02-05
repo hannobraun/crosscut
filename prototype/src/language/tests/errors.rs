5b3a48023858b1573b4b1a93246e8168b75c8d1e
Hanno Braun <hello@hannobraun.com>
2025-02-05 12:39:49 +0100

    Update name of test

--- prototype/src/language/tests/errors.rs
+++ prototype/src/language/tests/errors.rs
@@ -25,7 +25,7 @@ fn number_literal_wrong_input() {
 }
 
 #[test]
-fn unresolved_identifier() {
+fn unresolved_syntax_node() {
     // If an identifier does not refer to a known function, that should result
     // in an error.
 
