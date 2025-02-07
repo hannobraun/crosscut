9fcf7e5de2da25766d7bb3eda52c9bd0cd429726
Hanno Braun <hello@hannobraun.com>
2025-02-07 09:04:26 +0100

    Update name of struct field

--- src/language/code/expressions.rs
+++ src/language/code/expressions.rs
@@ -17,21 +17,21 @@ impl Expression {
     ) -> ExpressionDisplay<'r> {
         ExpressionDisplay {
             expression: self,
-            host: package,
+            package,
         }
     }
 }
 
 pub struct ExpressionDisplay<'r> {
     expression: &'r Expression,
-    host: &'r Package,
+    package: &'r Package,
 }
 
 impl fmt::Display for ExpressionDisplay<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.expression {
             Expression::HostFunction { id } => {
-                let name = self.host.function_name_by_id(id);
+                let name = self.package.function_name_by_id(id);
                 write!(f, "{name}")
             }
             Expression::IntrinsicFunction { function } => {
