d3782ab8d8090dc87edfedd54cdefc2b3973259d
Hanno Braun <hello@hannobraun.com>
2025-02-09 12:42:17 +0100

    Remove stale comment

--- src/language/tests/functions.rs
+++ src/language/tests/functions.rs
@@ -26,18 +26,6 @@ fn define_and_evaluate_function() {
         }
     };
 
-    // Just conjuring a `NodePath` out of thin air like this, only works because
-    // it is essentially equivalent to the `NodeHash` we already have. This
-    // won't be the case for much longer.
-    //
-    // And while we can't get a `NodePath` from `Value::Function` (this would
-    // create a circular dependency in how the hashes need to be computed), the
-    // good new is that `Evaluator` has all the info we need. It just doesn't
-    // return it when stepping.
-    //
-    // If it did, and all the intermediate layers passed on this information, we
-    // have all we need to construct the `NodePath` for the function body here,
-    // now and going forward.
     language.evaluate(path);
     assert_eq!(
         language.step_until_finished().map(|value| value.inner),
