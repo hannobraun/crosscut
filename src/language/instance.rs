7b9ac786aec28edc731184a6fbd560aceb224bac
Hanno Braun <hello@hannobraun.com>
2025-02-10 13:08:06 +0100

    Remove redundant code

--- src/language/instance.rs
+++ src/language/instance.rs
@@ -18,8 +18,7 @@ impl Language {
         let codebase = Codebase::new();
         let editor = Editor::new(&codebase);
 
-        let mut evaluator = Evaluator::new(codebase.root().path, &codebase);
-        evaluator.reset(&codebase);
+        let evaluator = Evaluator::new(codebase.root().path, &codebase);
 
         Self {
             codebase,
