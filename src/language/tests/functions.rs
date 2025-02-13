8b08bcbaaa0cb87dcc7ae14804b674b5e3c91b63
Hanno Braun <hello@hannobraun.com>
2025-02-13 14:34:43 +0100

    Prepare for follow-on change

--- src/language/tests/functions.rs
+++ src/language/tests/functions.rs
@@ -42,12 +42,19 @@ fn self_recursion() {
 
     language.evaluate(path, Value::Nothing);
 
-    let expected = StepResult::Running {
-        output: Value::Integer { value: 127 },
-    };
-    assert_eq!(language.step(), expected);
+    assert_eq!(
+        language.step(),
+        StepResult::Running {
+            output: Value::Integer { value: 127 },
+        }
+    );
     assert_eq!(language.step(), StepResult::Recursing);
-    assert_eq!(language.step(), expected);
+    assert_eq!(
+        language.step(),
+        StepResult::Running {
+            output: Value::Integer { value: 127 },
+        }
+    );
 }
 
 #[test]
