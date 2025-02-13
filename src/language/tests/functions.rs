359d18d54a2c66dab2bc0440ec6055fad0fbd30a
Hanno Braun <hello@hannobraun.com>
2025-02-13 15:16:48 +0100

    Prepare for follow-on change

--- src/language/tests/functions.rs
+++ src/language/tests/functions.rs
@@ -68,7 +68,7 @@ fn empty_function() {
         Some(&CodeError::FunctionWithoutBody),
     );
 
-    assert!(matches!(language.step(), StepResult::Error));
+    assert!(matches!(language.step(), StepResult::Error { .. }));
 }
 
 pub trait IntoFunctionBody {
