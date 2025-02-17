395f405709e0a223c8eed20b15f13caeb7dd6029
Hanno Braun <hello@hannobraun.com>
2025-02-17 13:28:50 +0100

    Prepare for follow-on change

--- src/language/code/expressions.rs
+++ src/language/code/expressions.rs
@@ -31,7 +31,8 @@ impl fmt::Display for ExpressionDisplay<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self.expression {
             Expression::HostFunction { id } => {
-                let name = self.resolver.function_name_by_id(id);
+                let resolver = self.resolver;
+                let name = resolver.function_name_by_id(id);
                 write!(f, "{name}")
             }
             Expression::IntrinsicFunction { intrinsic } => {
