299b464565e4a794c1cf5f4350382ca9bf516716
Hanno Braun <hello@hannobraun.com>
2025-03-06 11:59:49 +0100

    Sort intrinsic tests alphabetically

--- src/language/tests/intrinsic_functions.rs
+++ src/language/tests/intrinsic_functions.rs
@@ -1,6 +1,22 @@
 use crate::language::{instance::Language, runtime::Value};
 
 #[test]
+fn eval() {
+    // The `eval` function takes a function argument and evaluates that.
+    //
+    // So far, the `eval` function can only pass `nothing` to the evaluated
+    // function. Eventually, it should be able to pass any argument.
+
+    let mut language = Language::new();
+
+    language.on_code("127 fn eval");
+    assert_eq!(
+        language.step_until_finished(),
+        Ok(Value::Integer { value: 127 }),
+    );
+}
+
+#[test]
 fn number_literal() {
     // A number literal is a function that takes `nothing` and returns the
     // number it represents.
@@ -34,22 +50,6 @@ fn identity_integer() {
     let mut language = Language::new();
 
     language.on_code("127 identity");
-    assert_eq!(
-        language.step_until_finished(),
-        Ok(Value::Integer { value: 127 }),
-    );
-}
-
-#[test]
-fn eval() {
-    // The `eval` function takes a function argument and evaluates that.
-    //
-    // So far, the `eval` function can only pass `nothing` to the evaluated
-    // function. Eventually, it should be able to pass any argument.
-
-    let mut language = Language::new();
-
-    language.on_code("127 fn eval");
     assert_eq!(
         language.step_until_finished(),
         Ok(Value::Integer { value: 127 }),
