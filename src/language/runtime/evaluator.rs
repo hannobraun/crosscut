d114409e734b09f51dc27de1f987bb664247561c
Hanno Braun <hello@hannobraun.com>
2025-03-16 10:54:58 +0100

    Remove redundant `.clone()`

--- src/language/runtime/evaluator.rs
+++ src/language/runtime/evaluator.rs
@@ -270,7 +270,7 @@ impl Evaluator {
                                         active_value => {
                                             self.unexpected_input(
                                                 Type::Nothing,
-                                                active_value.clone(),
+                                                active_value,
                                                 path,
                                             );
                                             self.eval_stack.push(node);
