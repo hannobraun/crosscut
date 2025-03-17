447190dfdaa9c748ba7c84044b01b1ddd302c351
Hanno Braun <hello@hannobraun.com>
2025-03-17 11:41:46 +0100

    Prepare to add parent to `NodePath`

--- src/language/code/tree.rs
+++ src/language/code/tree.rs
@@ -15,16 +15,21 @@ impl SyntaxTree {
         nodes: &Nodes,
     ) -> Option<NodePath> {
         let mut to_search = Vec::new();
-        to_search.push(self.root.hash);
+        to_search.push(self.root);
 
         while let Some(hash) = to_search.pop() {
-            let node = nodes.get(&hash);
+            let node = nodes.get(&hash.hash);
 
             if node.children().contains(&child.hash) {
-                return Some(NodePath { hash });
+                return Some(hash);
             }
 
-            to_search.extend(node.children());
+            to_search.extend(
+                node.children()
+                    .iter()
+                    .copied()
+                    .map(|hash| NodePath { hash }),
+            );
         }
 
         None
