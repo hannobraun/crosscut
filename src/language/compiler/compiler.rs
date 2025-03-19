38eb3989881bc98fea364c6bcc754a77b6829df1
Hanno Braun <hello@hannobraun.com>
2025-03-19 13:11:53 +0100

    Prepare for follow-on change

--- src/language/compiler/compiler.rs
+++ src/language/compiler/compiler.rs
@@ -190,7 +190,7 @@ fn replace_node_and_update_parents(
         let hash = change_set.add(node);
 
         let path = NodePath { hash };
-        let path = change_set.replace(&next_to_replace, path);
+        change_set.replace(&next_to_replace, path.clone());
 
         previous_replacement = hash;
         initial_replacement = initial_replacement.or(Some(path.clone()));
