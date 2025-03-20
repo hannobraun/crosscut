d9153d119a21033fbd25736a8737403034f00b87
Hanno Braun <hello@hannobraun.com>
2025-03-20 12:45:57 +0100

    Simplify doc comment

--- src/language/code/nodes/hash.rs
+++ src/language/code/nodes/hash.rs
@@ -15,20 +15,10 @@ use super::Node;
 /// This is not unique, because the syntax tree can contain identical sub-trees
 /// in various places, each with a root node that would have identical hashes.
 ///
-/// To guarantee uniqueness, we also need the position of the node within the
-/// syntax tree, which includes the parent. But a node's parent already includes
-/// its own children, which would cause a circular dependency when computing the
-/// hash.
+/// If you need to uniquely identify a node within a syntax tree, please use
+/// [`NodePath`].
 ///
-/// To solve this issue, we have [`NodePath`], which after the hashes have been
-/// constructed bottom-up, is constructed top-down, from the root of the syntax
-/// tree.
-///
-/// ## Implementation Note
-///
-/// Not everything documented here, especially the relation to [`NodePath`], is
-/// implemented yet. I wanted to paint a complete picture of how this fits into
-/// my current plans, even though those are not fully realized yet.
+/// [`NodePath`]: super::NodePath
 #[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable)]
 pub struct NodeHash {
     hash: [u8; 32],
