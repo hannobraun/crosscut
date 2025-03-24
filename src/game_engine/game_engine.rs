a13a632feecdcd556cb2cca0ee2e2f1f6c7a95ad
Hanno Braun <hello@hannobraun.com>
2025-03-24 11:06:15 +0100

    Simplify

--- src/game_engine/game_engine.rs
+++ src/game_engine/game_engine.rs
@@ -109,7 +109,7 @@ where
                     // We're not interested in intermediate values here.
                     continue;
                 }
-                RuntimeState::Effect { effect, path: _ } => match effect {
+                RuntimeState::Effect { effect, .. } => match effect {
                     Effect::ApplyHostFunction { id, input } => {
                         self.apply_host_function(id, input);
                         continue;
