69e827eb3b2ded3e62ca8cd8aca7d90824cd287a
Hanno Braun <hello@hannobraun.com>
2025-03-27 13:22:03 +0100

    Remove redundant `#[allow(unused)]`

--- src/language/code/literal.rs
+++ src/language/code/literal.rs
@@ -1,9 +1,6 @@
 #[derive(Clone, Debug, Eq, PartialEq, udigest::Digestable)]
 pub enum Literal {
-    #[allow(unused)] // temporary oddity; work to resolve it is ongoing
     Function,
-    Integer {
-        value: i32,
-    },
+    Integer { value: i32 },
     Tuple,
 }
