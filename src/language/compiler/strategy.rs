57d77dc98c2f729725e919b428ea14deb1a8fd19
Hanno Braun <hello@hannobraun.com>
2025-04-03 14:29:21 +0200

    Increase clarity

--- src/language/compiler/strategy.rs
+++ src/language/compiler/strategy.rs
@@ -4,5 +4,7 @@ pub struct ReplacementStrategy {
     pub next_to_replace: NodePath,
     pub next_token: String,
     pub next_children: Children,
-    pub added_nodes: Vec<(NodePath, NodeHash, Option<CodeError>)>,
+    pub added_nodes: Vec<NodeAddedDuringReplacement>,
 }
+
+pub type NodeAddedDuringReplacement = (NodePath, NodeHash, Option<CodeError>);
