6e8ffef846919de1f18ad55007b98608f21e1698
Hanno Braun <hello@hannobraun.com>
2025-04-03 15:01:42 +0200

    Consolidate replacement code in `replace` module

--- src/language/compiler/replace.rs
+++ src/language/compiler/replace.rs
@@ -1,6 +1,90 @@
-use crate::language::code::{Children, CodeError, NodeHash, NodePath};
+use crate::language::{
+    code::{Children, CodeError, Errors, NewChangeSet, NodeHash, NodePath},
+    packages::Packages,
+};
 
 use super::token::Token;
+
+pub fn replace_node_and_update_parents(
+    to_replace: &NodePath,
+    replacement_token: &str,
+    children: Children,
+    packages: &Packages,
+    change_set: &mut NewChangeSet,
+    errors: &mut Errors,
+) -> NodePath {
+    let mut strategy = ReplacementStrategy {
+        next_to_replace: to_replace.clone(),
+        next_token: replacement_token.to_string(),
+        next_children: children,
+        added_nodes: Vec::new(),
+    };
+
+    loop {
+        let token = strategy.next_action();
+        let (node, maybe_error) = token.compile(change_set.nodes(), packages);
+
+        let added = change_set.add(node);
+
+        strategy.added_nodes.push(NodeAddedDuringReplacement {
+            replaced: strategy.next_to_replace.clone(),
+            added,
+            maybe_error,
+        });
+
+        if let Some(parent_path) = strategy.next_to_replace.parent().cloned() {
+            let parent_node = change_set.nodes().get(parent_path.hash());
+
+            strategy.next_token = parent_node.to_token(packages);
+            strategy.next_children = parent_node.to_children();
+
+            strategy
+                .next_children
+                .replace(strategy.next_to_replace.hash(), [added]);
+
+            strategy.next_to_replace = parent_path;
+
+            continue;
+        } else {
+            break;
+        };
+    }
+
+    let mut initial_replacement = None;
+    let mut parent = None;
+
+    while let Some(NodeAddedDuringReplacement {
+        replaced,
+        added,
+        maybe_error,
+    }) = strategy.added_nodes.pop()
+    {
+        let path = NodePath::new(
+            added,
+            parent,
+            replaced.sibling_index(),
+            change_set.nodes(),
+        );
+        parent = Some(path.clone());
+
+        change_set.replace(&replaced, &path);
+
+        initial_replacement = Some(path.clone());
+
+        if let Some(error) = maybe_error {
+            errors.insert(*path.hash(), error);
+        }
+    }
+
+    let Some(path) = initial_replacement else {
+        unreachable!(
+            "The loop above is executed at least once. The variable must have \
+            been set."
+        );
+    };
+
+    path
+}
 
 pub struct ReplacementStrategy {
     pub next_to_replace: NodePath,
