ad814b736984f91a5aefccde0a0fa79c2a587a45
Hanno Braun <hello@hannobraun.com>
2025-04-10 13:34:48 +0200

    Document `SiblingIndex`

--- src/language/code/nodes/path.rs
+++ src/language/code/nodes/path.rs
@@ -144,6 +144,17 @@ impl NodePath {
     }
 }
 
+/// # The index of a node among its siblings
+///
+/// ## Implementation Note
+///
+/// I'm a bit concerned with the use of `usize` here, as it could lead to
+/// problems when serializing `Codebase`. But using something else makes some
+/// other code much harder to write. I'd basically have to re-implement
+/// `iter::Enumerate`, including its implementation of `DoubleEndedIterator, for
+/// `u32` or whatever.
+///
+/// For now, this works. But it might have to change going forward.
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable)]
 pub struct SiblingIndex {
     pub index: usize,
