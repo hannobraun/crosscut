ef6e011cac3456f5abe371da2979bfdb5b419ee6
Hanno Braun <hello@hannobraun.com>
2025-04-23 22:04:48 +0200

    Port tests to prefix syntax

--- src/language/tests/editing.rs
+++ src/language/tests/editing.rs
@@ -8,7 +8,7 @@ fn update_on_every_character() {
     // has finished running, as is the case here, it should also reset the
     // interpreter, so the next step will run the new code.
 
-    let mut language = Language::postfix();
+    let mut language = Language::new();
 
     language.on_code("1");
     assert_eq!(
@@ -28,7 +28,7 @@ fn update_after_removing_character() {
     // Removing a character should have an immediate effect on the program, just
     // like adding one.
 
-    let mut language = Language::postfix();
+    let mut language = Language::new();
 
     language.on_code("127");
     assert_eq!(
@@ -54,7 +54,7 @@ fn update_after_removing_all_characters() {
     // Removing all characters should have an immediate effect on the program,
     // just like any other edits.
 
-    let mut language = Language::postfix();
+    let mut language = Language::new();
 
     language.on_code("1");
     assert_eq!(
