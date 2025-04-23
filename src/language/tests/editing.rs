b2184eb053616306a3a39c176e71d10fed59eae8
Hanno Braun <hello@hannobraun.com>
2025-04-23 11:45:54 +0200

    Simplify

--- src/language/tests/editing.rs
+++ src/language/tests/editing.rs
@@ -231,21 +231,18 @@ fn split_node_if_adding_sibling_while_cursor_is_in_the_middle() {
     );
 
     let [a, b] = root
-        .node
-        .to_children()
-        .into_iter()
-        .map(|hash| language.codebase().nodes().get(&hash))
+        .children(language.codebase().nodes())
         .collect_array()
         .unwrap();
     assert_eq!(
-        a,
+        a.node,
         &Node::Error {
             node: "a".to_string(),
             children: Children::new([]),
         },
     );
     assert_eq!(
-        b,
+        b.node,
         &Node::Error {
             node: "b".to_string(),
             children: Children::new([]),
