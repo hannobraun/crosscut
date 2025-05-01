c2a19fd586fece15b995be0b1234f1e4f3da966f
Hanno Braun <hello@hannobraun.com>
2025-05-01 22:43:29 +0200

    Prepare to simplify `Expression::Error`

--- src/language/editor/tests/navigate.rs
+++ src/language/editor/tests/navigate.rs
@@ -294,7 +294,7 @@ fn navigate_up_to_previous_sibling() {
     {
         let mut compiler = Compiler::new(&mut codebase);
 
-        compiler.replace(&compiler.codebase().root().path, "root", &packages);
+        compiler.replace(&compiler.codebase().root().path, "tuple", &packages);
 
         compiler.insert_child(compiler.codebase().root().path, "a", &packages);
         compiler.insert_child(compiler.codebase().root().path, "b", &packages);
