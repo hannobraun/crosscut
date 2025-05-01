0ade8904c2cdf41e0c9e01f4720dd9710b1ae266
Hanno Braun <hello@hannobraun.com>
2025-05-01 22:33:59 +0200

    Refactor test to improve clarity

--- src/language/editor/tests/navigate.rs
+++ src/language/editor/tests/navigate.rs
@@ -161,8 +161,11 @@ fn navigate_left_to_parent() {
     let child = {
         let mut compiler = Compiler::new(&mut codebase);
 
-        let parent =
-            compiler.replace(&compiler.codebase().root().path, "a", &packages);
+        let parent = compiler.replace(
+            &compiler.codebase().root().path,
+            "parent",
+            &packages,
+        );
         compiler.insert_child(parent, "b", &packages)
     };
 
@@ -185,7 +188,7 @@ fn navigate_left_to_parent() {
         editor.cursor(),
         &Cursor {
             path: codebase.root().path,
-            index: 1
+            index: "parent".len(),
         },
     );
 }
