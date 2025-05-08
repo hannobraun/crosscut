462392cb3ecd4ba6c7996912b26004f2e849998c
Hanno Braun <hello@hannobraun.com>
2025-05-08 15:08:39 +0200

    Prepare to make `Parent` generic

--- src/language/code/nodes/path.rs
+++ src/language/code/nodes/path.rs
@@ -258,13 +258,92 @@ impl<T: SyntaxNode> udigest::Digestable for NodePath<T> {
     }
 }
 
-#[derive(
-    Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd, udigest::Digestable,
-)]
 pub struct Parent {
     hash: RawHash,
     sibling_index: SiblingIndex,
     parent: RawHash,
+}
+
+impl Copy for Parent {}
+
+impl Clone for Parent {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl Eq for Parent {}
+
+impl Ord for Parent {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.hash.cmp(&other.hash)
+    }
+}
+
+impl PartialEq for Parent {
+    fn eq(&self, other: &Self) -> bool {
+        let Self {
+            hash,
+            sibling_index,
+            parent,
+        } = self;
+
+        hash == &other.hash
+            && sibling_index == &other.sibling_index
+            && parent == &other.parent
+    }
+}
+
+impl PartialOrd for Parent {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl fmt::Debug for Parent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let Self {
+            hash,
+            sibling_index,
+            parent,
+        } = self;
+
+        f.debug_struct("Parent")
+            .field("hash", &hash)
+            .field("sibling_index", &sibling_index)
+            .field("parent", &parent)
+            .finish()
+    }
+}
+
+impl udigest::Digestable for Parent {
+    fn unambiguously_encode<B: udigest::Buffer>(
+        &self,
+        encoder: udigest::encoding::EncodeValue<B>,
+    ) {
+        let Self {
+            hash,
+            sibling_index,
+            parent,
+        } = self;
+
+        let mut encoder = encoder.encode_struct();
+
+        {
+            let encoder = encoder.add_field("hash");
+            hash.unambiguously_encode(encoder);
+        }
+        {
+            let encoder = encoder.add_field("sibling_index");
+            sibling_index.unambiguously_encode(encoder);
+        }
+        {
+            let encoder = encoder.add_field("parent");
+            parent.unambiguously_encode(encoder);
+        }
+
+        encoder.finish();
+    }
 }
 
 /// # The index of a node among its siblings
