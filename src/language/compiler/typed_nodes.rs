c48a4bda070a61d9dfef24983d18082f3ac9b6c2
Hanno Braun <hello@hannobraun.com>
2025-05-13 22:27:01 +0200

    Prepare for follow-on change

--- src/language/compiler/typed_nodes.rs
+++ src/language/compiler/typed_nodes.rs
@@ -4,7 +4,9 @@ pub struct Function;
 
 impl Function {
     pub fn to_node(&self, nodes: &mut Nodes) -> SyntaxNode {
-        let [parameter, body] = [nodes.insert(SyntaxNode::Empty); 2];
+        let parameter = nodes.insert(SyntaxNode::Empty);
+        let body = nodes.insert(SyntaxNode::Empty);
+
         SyntaxNode::Function { parameter, body }
     }
 }
