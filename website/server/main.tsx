553d2b4944ab02feffb9780b279b4db0e8ba9276
Hanno Braun <hello@hannobraun.com>
2025-05-15 20:57:53 +0200

    Disable analytics

--- website/server/main.tsx
+++ website/server/main.tsx
@@ -8,7 +8,10 @@ Deno.serve(async (request) => {
     const response = await servePage(request);
 
     // Not awaiting the request tracking, so it doesn't block the response.
-    trackRequest(request);
+    //
+    // Currently disabled, because analyzing the result was more of a bother
+    // than I was ready for.
+    // trackRequest(request);
 
     return response;
 });
@@ -76,22 +79,23 @@ async function servePage(request: Request) {
     });
 }
 
-async function trackRequest(request: Request) {
-    try {
-        const data = {
-            timestamp: new Date().toISOString(),
-            id: crypto.randomUUID(),
-            region: Deno.env.get("DENO_REGION"),
-            deploymentId: Deno.env.get("DENO_DEPLOYMENT_ID"),
-            method: request.method,
-            url: request.url,
-            userAgent: request.headers.get("user-agent"),
-            referrer: request.headers.get("referer"),
-        };
+// Currently disabled. Leaving it in, in case I change my mind.
+// async function trackRequest(request: Request) {
+//     try {
+//         const data = {
+//             timestamp: new Date().toISOString(),
+//             id: crypto.randomUUID(),
+//             region: Deno.env.get("DENO_REGION"),
+//             deploymentId: Deno.env.get("DENO_DEPLOYMENT_ID"),
+//             method: request.method,
+//             url: request.url,
+//             userAgent: request.headers.get("user-agent"),
+//             referrer: request.headers.get("referer"),
+//         };
 
-        const kv = await Deno.openKv();
-        await kv.set(["analytics", data.timestamp, data.id], data);
-    } catch (error) {
-        console.error("Failed to track request:", error);
-    }
-}
+//         const kv = await Deno.openKv();
+//         await kv.set(["analytics", data.timestamp, data.id], data);
+//     } catch (error) {
+//         console.error("Failed to track request:", error);
+//     }
+// }
