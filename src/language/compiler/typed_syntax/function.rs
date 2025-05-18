883c6f586a7981cecd88111a842a65134d7dda38
Hanno Braun <hello@hannobraun.com>
2025-05-19 01:37:53 +0200

    Make `Function` more flexible

--- src/language/compiler/typed_syntax/function.rs
+++ src/language/compiler/typed_syntax/function.rs
@@ -1,15 +1,24 @@
 use crate::language::code::{Children, Nodes, SyntaxNode};
 
-#[derive(Default)]
-pub struct Function {}
+pub struct Function {
+    pub parameter: SyntaxNode,
+}
 
 impl Function {
     pub fn into_syntax_node(self, nodes: &mut Nodes) -> SyntaxNode {
-        let parameter = nodes.insert(SyntaxNode::Binding {
-            name: "_".to_string(),
-        });
+        let parameter = nodes.insert(self.parameter);
         let body = Children::new([nodes.insert(SyntaxNode::Empty)]);
 
         SyntaxNode::Function { parameter, body }
+    }
+}
+
+impl Default for Function {
+    fn default() -> Self {
+        Self {
+            parameter: SyntaxNode::Binding {
+                name: "_".to_string(),
+            },
+        }
     }
 }
