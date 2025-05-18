d20c1c6c3726f853a6d9cdb2205357f1cf2df337
Hanno Braun <hello@hannobraun.com>
2025-05-19 00:39:54 +0200

    Move code to restore alphabetic order

--- src/language/compiler/typed_syntax/form.rs
+++ src/language/compiler/typed_syntax/form.rs
@@ -6,14 +6,14 @@ pub trait Form {
     type Form<T>;
 }
 
-pub struct Owned;
-
-impl Form for Owned {
-    type Form<T> = T;
-}
-
 pub struct NodeRef<'r>(PhantomData<&'r ()>);
 
 impl<'r> Form for NodeRef<'r> {
     type Form<T> = &'r NodeHash;
+}
+
+pub struct Owned;
+
+impl Form for Owned {
+    type Form<T> = T;
 }
