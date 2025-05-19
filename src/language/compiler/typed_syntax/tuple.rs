49f03a2c3c7e372d6a92ddeb66c58dea43163536
Hanno Braun <hello@hannobraun.com>
2025-05-19 10:52:07 +0200

    Prepare for follow-on change

--- src/language/compiler/typed_syntax/tuple.rs
+++ src/language/compiler/typed_syntax/tuple.rs
@@ -5,7 +5,7 @@ pub struct Tuple {}
 
 impl Tuple {
     pub fn into_syntax_node(self, nodes: &mut Nodes) -> SyntaxNode {
-        let values = [];
+        let values = [].map(|node| nodes.insert(node));
         let add_value = nodes.insert(SyntaxNode::AddNode);
 
         SyntaxNode::Tuple {
