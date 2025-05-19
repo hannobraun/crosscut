3e2321b2d0de384c5b673f9648c5a559c3c98766
Hanno Braun <hello@hannobraun.com>
2025-05-19 10:53:52 +0200

    Make `Tuple` more flexible

--- src/language/compiler/typed_syntax/tuple.rs
+++ src/language/compiler/typed_syntax/tuple.rs
@@ -1,8 +1,8 @@
 use crate::language::code::{ChildrenOwned, Nodes, SyntaxNode};
 
-#[derive(Default)]
 pub struct Tuple {
     pub values: Vec<SyntaxNode>,
+    pub add_value: SyntaxNode,
 }
 
 impl Tuple {
@@ -12,11 +12,20 @@ impl Tuple {
             .into_iter()
             .map(|node| nodes.insert(node))
             .collect::<Vec<_>>();
-        let add_value = nodes.insert(SyntaxNode::AddNode);
+        let add_value = nodes.insert(self.add_value);
 
         SyntaxNode::Tuple {
             values: ChildrenOwned::new(values),
             add_value,
+        }
+    }
+}
+
+impl Default for Tuple {
+    fn default() -> Self {
+        Self {
+            values: Vec::new(),
+            add_value: SyntaxNode::AddNode,
         }
     }
 }
