de5af76653768b91253e2bd3bd3c6a254b2327aa
Hanno Braun <hello@hannobraun.com>
2025-05-21 14:50:16 +0200

    Increase consistency between module names

--- src/language/code/mod.rs
+++ src/language/code/mod.rs
@@ -1,22 +1,22 @@
 mod changes;
 mod codebase;
+mod nodes_typed;
 mod nodes_uniform;
-mod typed_syntax;
 mod types;
 
 pub use self::{
     changes::{Changes, NewChangeSet},
     codebase::Codebase,
+    nodes_typed::{Apply, Function, Tuple, TypedNode},
     nodes_uniform::{
         ChildIndex, LocatedNode, NodeByHash, NodeHash, NodePath, Nodes,
         SyntaxNode,
     },
-    typed_syntax::{Apply, Function, Tuple, TypedNode},
     types::{Type, display_tuple},
 };
 
 #[cfg(test)]
-pub use self::typed_syntax::Expressions;
+pub use self::nodes_typed::Expressions;
 
 #[cfg(test)]
 mod tests;
