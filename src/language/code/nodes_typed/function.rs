ef127e097ad21336324b3d3b42cf28d41682f745
Hanno Braun <hello@hannobraun.com>
2025-05-23 13:56:05 +0200

    Add implementation note

--- src/language/code/nodes_typed/function.rs
+++ src/language/code/nodes_typed/function.rs
@@ -7,6 +7,21 @@ use super::Binding;
 
 pub struct Function<T: Form> {
     pub parameter: T::Form<Binding>,
+
+    /// # The body of the function
+    ///
+    /// ## Implementation Note
+    ///
+    /// This refers to all expressions in the function bodies by hash,
+    /// regardless of what form the other fields have. This is not as type-safe
+    /// as I would want these typed node types to be, but it's necessary. The
+    /// hash is required to construct function values from this, at runtime.
+    ///
+    /// Maybe the type safety can be taken care of by an accessor method, which
+    /// returns `Children`, which could get a type parameter and a conversion
+    /// function into this type parameter.
+    ///
+    /// - Hanno Braun
     pub body: Vec<T::Form<NodeHash>>,
 }
 
