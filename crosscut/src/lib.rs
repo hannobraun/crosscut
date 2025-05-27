20d2fb4317c548cd9d84eb6a7f07cf5d09fa8aae
Hanno Braun <hello@hannobraun.com>
2025-05-27 20:50:33 +0200

    Add `start_and_wait` function

--- crosscut/src/lib.rs
+++ crosscut/src/lib.rs
@@ -8,3 +8,25 @@ pub mod io;
 pub mod language;
 pub mod threads;
 pub mod util;
+
+pub fn start_and_wait() -> anyhow::Result<()> {
+    let threads = threads::start()?;
+
+    // This call is going to block until the user requests a shutdown via the
+    // game I/O, or any of the other threads shut down.
+    io::game_engine::start_and_wait(threads.game_input, threads.game_output)?;
+
+    // At this point, the shutdown should be in progress. None of these calls
+    // should block for long, if at all. The purpose of still joining all
+    // threads is just to get any error they might have produced.
+    //
+    // And let's join all threads first before printing any errors. Just to make
+    // sure that they have ended, and the terminal is not still in raw mode or
+    // something, when we start printing here.
+    let results = threads.handles.map(|handle| handle.join());
+    for result in results {
+        result?;
+    }
+
+    Ok(())
+}
--- crosscut/src/main.rs
+++ crosscut/src/main.rs
@@ -1,24 +1,3 @@
 fn main() -> anyhow::Result<()> {
-    let threads = crosscut::threads::start()?;
-
-    // This call is going to block until the user requests a shutdown via the
-    // game I/O, or any of the other threads shut down.
-    crosscut::io::game_engine::start_and_wait(
-        threads.game_input,
-        threads.game_output,
-    )?;
-
-    // At this point, the shutdown should be in progress. None of these calls
-    // should block for long, if at all. The purpose of still joining all
-    // threads is just to get any error they might have produced.
-    //
-    // And let's join all threads first before printing any errors. Just to make
-    // sure that they have ended, and the terminal is not still in raw mode or
-    // something, when we start printing here.
-    let results = threads.handles.map(|handle| handle.join());
-    for result in results {
-        result?;
-    }
-
-    Ok(())
+    crosscut::start_and_wait()
 }
