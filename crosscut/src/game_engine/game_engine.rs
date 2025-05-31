a781022d6cf596b4cc4241eea14dc13bfd5ca258
Hanno Braun <hello@hannobraun.com>
2025-05-31 21:51:25 +0200

    Prepare for follow-on change

--- crosscut/src/game_engine/game_engine.rs
+++ crosscut/src/game_engine/game_engine.rs
@@ -56,14 +56,17 @@ where
         match self.editor_input.on_input(input) {
             Some(EditorInputOrCommand::Input { input }) => {
                 self.language.on_input(input);
+                self.game
+                    .on_editor_input(&mut self.language, &mut self.game_output);
             }
             Some(EditorInputOrCommand::Command { command }) => {
                 self.language.on_command(command)?;
+                self.game
+                    .on_editor_input(&mut self.language, &mut self.game_output);
             }
             None => {}
         }
-        self.game
-            .on_editor_input(&mut self.language, &mut self.game_output);
+
         self.render_editor()?;
 
         Ok(())
