2c98d19a48cff0dc1bbda39beb28ca1f41affe90
Hanno Braun <hello@hannobraun.com>
2025-06-06 09:35:41 +0200

    Prepare to support camera projections

--- crosscut/src/game_engine/camera.rs
+++ crosscut/src/game_engine/camera.rs
@@ -1,13 +1,22 @@
-#[derive(Default)]
-pub struct Camera {}
+pub struct Camera {
+    transform: [[f32; 4]; 4],
+}
 
 impl Camera {
     pub fn to_transform(&self) -> [[f32; 4]; 4] {
-        [
-            [1., 0., 0., 0.],
-            [0., 1., 0., 0.],
-            [0., 0., 1., 0.],
-            [0., 0., 0., 1.],
-        ]
+        self.transform
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self {
+            transform: [
+                [1., 0., 0., 0.],
+                [0., 1., 0., 0.],
+                [0., 0., 1., 0.],
+                [0., 0., 0., 1.],
+            ],
+        }
     }
 }
