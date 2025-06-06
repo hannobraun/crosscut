95320b8c708adfba859e5482f35d74844a329c10
Hanno Braun <hello@hannobraun.com>
2025-06-06 09:58:01 +0200

    Update name of variable

--- games/snake/src/main.rs
+++ games/snake/src/main.rs
@@ -1,6 +1,6 @@
 mod game;
 
 fn main() -> anyhow::Result<()> {
-    let game = Box::new(game::SnakeStart::default());
-    crosscut::start_and_wait(game)
+    let game_start = Box::new(game::SnakeStart::default());
+    crosscut::start_and_wait(game_start)
 }
