59e14df0381c1f6a4645aee587706f3f97dc9425
Hanno Braun <hello@hannobraun.com>
2025-06-09 12:22:23 +0200

    Add `Quads::new`

--- crosscut/src/game_engine/graphics/renderer.rs
+++ crosscut/src/game_engine/graphics/renderer.rs
@@ -5,7 +5,7 @@ use winit::window::Window;
 
 use crate::{
     Camera, Instance,
-    game_engine::graphics::quads::{Quads, Uniforms, Vertex},
+    game_engine::graphics::quads::{Quads, Uniforms},
 };
 
 pub struct Renderer {
@@ -51,129 +51,7 @@ impl Renderer {
             })?;
         surface.configure(&device, &surface_config);
 
-        let uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: None,
-            size: Uniforms::size(),
-            usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
-            mapped_at_creation: false,
-        });
-        queue.write_buffer(
-            &uniform_buffer,
-            0,
-            bytemuck::cast_slice(&[Uniforms::default()]),
-        );
-
-        let bind_group_layout =
-            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                label: None,
-                entries: &[wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None,
-                }],
-            });
-
-        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            label: None,
-            layout: &bind_group_layout,
-            entries: &[wgpu::BindGroupEntry {
-                binding: 0,
-                resource: uniform_buffer.as_entire_binding(),
-            }],
-        });
-
-        let pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: None,
-                bind_group_layouts: &[&bind_group_layout],
-                push_constant_ranges: &[],
-            });
-
-        let shader =
-            device.create_shader_module(wgpu::ShaderModuleDescriptor {
-                label: None,
-                source: wgpu::ShaderSource::Wgsl(
-                    include_str!("shader.wgsl").into(),
-                ),
-            });
-
-        let pipeline =
-            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                label: None,
-                layout: Some(&pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: Some("vert_main"),
-                    compilation_options:
-                        wgpu::PipelineCompilationOptions::default(),
-                    buffers: &[Vertex::layout(), Instance::layout()],
-                },
-                primitive: wgpu::PrimitiveState {
-                    topology: wgpu::PrimitiveTopology::TriangleStrip,
-                    strip_index_format: None,
-                    front_face: wgpu::FrontFace::Ccw,
-                    cull_mode: None,
-                    unclipped_depth: false,
-                    polygon_mode: wgpu::PolygonMode::Fill,
-                    conservative: false,
-                },
-                depth_stencil: None,
-                fragment: Some(wgpu::FragmentState {
-                    module: &shader,
-                    entry_point: Some("frag_main"),
-                    compilation_options:
-                        wgpu::PipelineCompilationOptions::default(),
-                    targets: &[Some(wgpu::ColorTargetState {
-                        format: surface_config.format,
-                        blend: None,
-                        write_mask: wgpu::ColorWrites::ALL,
-                    })],
-                }),
-                multisample: wgpu::MultisampleState::default(),
-                multiview: None,
-                cache: None,
-            });
-
-        let vertices = [[0.5, -0.5], [0.5, 0.5], [-0.5, -0.5], [-0.5, 0.5]]
-            .map(|[x, y]| {
-                let position = [x, y, 0.];
-                Vertex { position }
-            });
-
-        let vertex_buffer = device.create_buffer(&Vertex::buffer_descriptor());
-        queue.write_buffer(&vertex_buffer, 0, bytemuck::cast_slice(&vertices));
-
-        let num_vertices: u32 = {
-            let Ok(len) = vertices.len().try_into() else {
-                unreachable!(
-                    "Number of vertices defined here fits into an `u32`."
-                );
-            };
-
-            len
-        };
-
-        {
-            let num_vertices: u64 = num_vertices.into();
-            assert!(num_vertices <= Vertex::MAX_NUM);
-        }
-
-        let instance_buffer =
-            device.create_buffer(&Instance::buffer_descriptor());
-
-        let quads = Quads {
-            pipeline,
-            bind_group,
-            uniform_buffer,
-            vertex_buffer,
-            num_vertices,
-            instance_buffer,
-        };
+        let quads = Quads::new(&device, &queue, &surface_config);
 
         Ok(Self {
             surface,
