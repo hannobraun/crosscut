2d3d8a288fb874ecb691eae29c1b1eadf94f44ab
Hanno Braun <hello@hannobraun.com>
2025-06-13 19:41:04 +0200

    Update name of variable

--- crosscut/src/main.rs
+++ crosscut/src/main.rs
@@ -1,4 +1,4 @@
 fn main() -> anyhow::Result<()> {
-    let game_start = Box::new(crosscut::PureCrosscutGameInit::default());
-    crosscut::start_and_wait(game_start)
+    let init = Box::new(crosscut::PureCrosscutGameInit::default());
+    crosscut::start_and_wait(init)
 }
