4cfb7214ebf1aed9043c9248b9194e39cdfa86e6
Hanno Braun <hello@hannobraun.com>
2025-06-14 13:34:55 +0200

    Implement `-` intrinsic

--- crosscut/src/language/runtime/intrinsics.rs
+++ crosscut/src/language/runtime/intrinsics.rs
@@ -29,6 +29,26 @@ pub fn apply_intrinsic_function(
                 actual: input.clone(),
             }))
         }
+        "-" => {
+            if let Value::Tuple { values } = &input {
+                if let Some([a, b]) = values.iter().collect_array() {
+                    if let [
+                        Value::Integer { value: a },
+                        Value::Integer { value: b },
+                    ] = [a, b]
+                    {
+                        return Some(Ok(Value::Integer { value: *a - *b }));
+                    }
+                }
+            }
+
+            Some(Err(Effect::UnexpectedInput {
+                expected: Type::Tuple {
+                    values: vec![Type::Integer, Type::Integer],
+                },
+                actual: input.clone(),
+            }))
+        }
         "drop" => Some(Ok(Value::nothing())),
         "identity" => Some(Ok(input.clone())),
         _ => None,
