819a8d9a91443d3f4f0a4fbada62173d80bfe998
Hanno Braun <hello@hannobraun.com>
2025-06-24 11:58:05 +0200

    Add comment

--- crosscut/src/language/import.rs
+++ crosscut/src/language/import.rs
@@ -65,6 +65,9 @@ fn handle_navigating_past_add_nodes(
         };
 
         if indent >= parent_indent {
+            // This could not have been true in the first iteration of the loop,
+            // as per the check above. Which means the code belove has done its
+            // job and we're done.
             break;
         }
 
