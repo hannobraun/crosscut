a2596ef2be0433604a0b62cdef4eb3b631c730de
Hanno Braun <hello@hannobraun.com>
2025-07-08 11:09:39 +0200

    Remove unused code

--- website/server/main.tsx
+++ website/server/main.tsx
@@ -1,9 +1,5 @@
 import * as http from "@std/http";
 
-import * as content from "./content.ts";
-import * as response from "./response.ts";
-import { dailyNotesPage, singleDailyNotePage } from "./templates.tsx";
-
 Deno.serve(async (request) => {
     const response = await servePage(request);
 
@@ -16,7 +12,7 @@ Deno.serve(async (request) => {
     return response;
 });
 
-async function servePage(request: Request) {
+function servePage(request: Request) {
     const url = new URL(request.url);
 
     if (
@@ -35,43 +31,6 @@ async function servePage(request: Request) {
             `${url.origin}/daily`,
             307,
         );
-    }
-    if (url.pathname == "/daily/") {
-        return Response.redirect(
-            `${url.origin}/daily`,
-            307,
-        );
-    }
-
-    if (url.pathname == "/daily") {
-        const dates = await content.listDailyNotes();
-        const page = dailyNotesPage(dates);
-        return response.page(page);
-    }
-
-    const dailyDateWithSlash = url.pathname.match(
-        /^\/daily\/(\d{4}-\d{2}-\d{2})\/$/,
-    );
-    if (dailyDateWithSlash && dailyDateWithSlash[1]) {
-        return Response.redirect(
-            `${url.origin}/daily/${dailyDateWithSlash[1]}`,
-            307,
-        );
-    }
-
-    const dailyDateWithNoSlash = url.pathname.match(
-        /^\/daily\/(\d{4}-\d{2}-\d{2})$/,
-    );
-    if (dailyDateWithNoSlash && dailyDateWithNoSlash[1]) {
-        const date = dailyDateWithNoSlash[1];
-        const path = `content/daily/${date}.md`;
-        const md = await Deno.readTextFile(path);
-
-        const dates = await content.listDailyNotes();
-
-        const page = singleDailyNotePage(date, md, dates);
-
-        return response.page(page);
     }
 
     return http.serveDir(request, {
--- website/server/response.ts
+++
@@ -1,9 +0,0 @@
-export const page = (html: { toString(): string }) => {
-    return new Response(
-        html.toString(),
-        {
-            status: 200,
-            headers: { "Content-Type": "text/html" },
-        },
-    );
-};
