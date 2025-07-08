aa78562810cf8de01949f176bb6181111f50eea1
Hanno Braun <hello@hannobraun.com>
2025-07-08 11:13:37 +0200

    Remove unused code

--- website/server/main.tsx
+++ website/server/main.tsx
@@ -2,13 +2,6 @@ import * as http from "@std/http";
 
 Deno.serve(async (request) => {
     const response = await servePage(request);
-
-    // Not awaiting the request tracking, so it doesn't block the response.
-    //
-    // Currently disabled, because analyzing the result was more of a bother
-    // than I was ready for.
-    // trackRequest(request);
-
     return response;
 });
 
@@ -37,24 +30,3 @@ function servePage(request: Request) {
         fsRoot: "static",
     });
 }
-
-// Currently disabled. Leaving it in, in case I change my mind.
-// async function trackRequest(request: Request) {
-//     try {
-//         const data = {
-//             timestamp: new Date().toISOString(),
-//             id: crypto.randomUUID(),
-//             region: Deno.env.get("DENO_REGION"),
-//             deploymentId: Deno.env.get("DENO_DEPLOYMENT_ID"),
-//             method: request.method,
-//             url: request.url,
-//             userAgent: request.headers.get("user-agent"),
-//             referrer: request.headers.get("referer"),
-//         };
-
-//         const kv = await Deno.openKv();
-//         await kv.set(["analytics", data.timestamp, data.id], data);
-//     } catch (error) {
-//         console.error("Failed to track request:", error);
-//     }
-// }
